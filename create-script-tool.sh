c169b002e95e8994c91b192875bd1c7c940d5a96
ukijumotahaneniarukenia <mrchildrenkh1008@gmail.com>
2020-02-11 21:18:54 +0900

    スケッチ

--- create-script-tool.sh
+++ create-script-tool.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 while read tgt;do
   LFT=$(echo $tgt)
-  RGT="/usr/local/bin/"$(echo $tgt | perl -pe 's;\..*$;;g;' | perl -pe 's;.*-;;g')"-myself"
+  RGT="$HOME/.local/bin/"$(echo $tgt | perl -pe 's;\..*$;;g;' | perl -pe 's;.*-;;g')""
   printf "ln -fsr %s %s\n" $LFT $RGT
 done < <(find ~/script-sketch -name "*" | grep -vP '\.git/' | grep -P 'DONE' ) | sort #>>~/.bashrc
