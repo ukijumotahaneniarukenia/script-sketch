d079adafc84ab98140e242b49aa03ba9f3526f97
ukijumotahaneniarukenia <mrchildrenkh1008@gmail.com>
2020-02-11 15:23:48 +0900

    スケッチ

--- git/00001-git-任意の単語に紐づくgithubレポジトリのリンク取得-github-repository-crawler-grc.sh
+++ git/00001-git-任意の単語に紐づくgithubレポジトリのリンク取得-github-repository-crawler-grc.sh
@@ -4,13 +4,15 @@ Q="$@"
 
 [ -z "$Q" ] && exit 1
 
-RT=$(curl -s "https://github.com/search?p=1&q=$Q" | grep -Po '(?<=data-search-type="Repositories">)[0-9]+[KMG]' | head -n1)
+RT=$(curl -s "https://github.com/search?p=1&q=$Q" | grep -Po '(?<=data-search-type="Repositories">)[0-9]+' | head -n1)
 MX_CNT=0
-[ -z $(echo $RT | grep -P 'G$') ] && MX_CNT=$(bc<<<"$(echo $RT | grep -Po '[0-9]+')*1000000000")
-[ -z $(echo $RT | grep -P 'M$') ] && MX_CNT=$(bc<<<"$(echo $RT | grep -Po '[0-9]+')*1000000")
-[ -z $(echo $RT | grep -P 'K$') ] && MX_CNT=$(bc<<<"$(echo $RT | grep -Po '[0-9]+')*1000")
+MX_CNT=$(echo $RT | grep -Po '[0-9]+')
+[ -z $(echo $RT | grep -P 'G$') ] || MX_CNT=$(bc<<<"$(echo $RT | grep -Po '[0-9]+')*1000000000")
+[ -z $(echo $RT | grep -P 'M$') ] || MX_CNT=$(bc<<<"$(echo $RT | grep -Po '[0-9]+')*1000000")
+[ -z $(echo $RT | grep -P 'K$') ] || MX_CNT=$(bc<<<"$(echo $RT | grep -Po '[0-9]+')*1000")
 
 for (( CNT=1;$CNT<=$MX_CNT;CNT++ ));do
+  echo trying $CNT round...
   curl -s "https://github.com/search?p=$CNT&q=$Q" | grep -oPi 'https?://github\.com(/[0-9a-z\-]{1,}){1,}' | \
   grep -vP '(about|contact|events|fluidicon|pricing|search|search/count|security|site/privacy|site/terms)$'
   sleep 2
