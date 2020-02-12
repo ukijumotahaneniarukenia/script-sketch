34082b6dd071fc9e2c3cb3ca9dade93f81c12658
ukijumotahaneniarukenia <mrchildrenkh1008@gmail.com>
2020-02-12 22:55:19 +0900

    スケッチ

--- create-script-tool.sh
+++ create-script-tool.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-mkdir -p $HOME/.local/bin
-
 while read tgt;do
+  DIR="$HOME/.local/bin/"$(echo $tgt | perl -pe 's;/[0-9]{5}.*;;g' | perl -pe 's;.*/;;')""
   LFT=$(echo $tgt)
   RGT="$HOME/.local/bin/"$(echo $tgt | perl -pe 's;/[0-9]{5}.*;;g' | perl -pe 's;.*/;;')"/"$(echo $tgt | perl -pe 's;\..*$;;g;' | perl -pe 's;.*-;;g')""
+  printf "mkdir -p %s\n" $DIR
   printf "ln -fsr %s %s\n" $LFT $RGT
-done < <(find ~/script-sketch -name "*" | grep -vP '\.git/' | grep -P 'DONE' ) | sort #>>~/.bashrc
+  printf "export PATH=%s:\$PATH\n" $DIR >>$HOME/.bashrc
+done < <(find $HOME/script-sketch -name "*" | grep -vP '\.git/' | grep -P 'DONE' ) | sh
+
+source $HOME/.bashrc
