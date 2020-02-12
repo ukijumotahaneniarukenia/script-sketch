e917a0b4128da182572a458119523355a8fd2907
ukijumotahaneniarukenia <mrchildrenkh1008@gmail.com>
2020-02-12 23:08:59 +0900

    スケッチ

--- perl/00002-perl-組み合わせ-combination-DONE-comb.pl
+++ perl/00002-perl-組み合わせ-combination-DONE-comb.pl
@@ -6,7 +6,7 @@ sub usage{
   my $msg=<<"EOS";
 Usage:
   LIB: apt install -y cpanminus && cpanm Math::Combinatorics
-  CMD: $0 {1..4} or echo {1..4} | $0
+  CMD: $0 2 {1..4} or echo 2 {1..4} | $0
   OUT:
 EOS
   print $msg,"\n";
@@ -15,7 +15,7 @@ EOS
 
 sub mock{
   eval {
-    print join("\n",map { join(" ", @$_) } permute(@_)),"\n"; #permute関数の戻り値がリファで返却されてくるので、デリファする
+    print join("\n",map { join(" ", @$_) } combine(shift(@_), @_)),"\n"; #combine関数の戻り値がリファで返却されてくるので、デリファする
   };
 
   if($@){
