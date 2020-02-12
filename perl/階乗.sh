aab4a3b6c9eee5ba5e1129a175d157bc58862b81
ukijumotahaneniarukenia <mrchildrenkh1008@gmail.com>
2020-02-12 23:24:40 +0900

    スケッチ

--- perl/階乗.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-seq 10 | xargs -I@ perl -e 'print @**2,"\n"'
--- perl/階乗_List_Util_reduce.pl
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env perl
-
-#https://perlmaven.com/factorial-in-perl
-$n=5;
-use List::Util qw(reduce);
-my $fact = reduce { $a * $b } 1 .. $n;
-print $fact,"\n";
