0d2f463c5a353b5b0bf944018aeeeca77d9062e8
ukijumotahaneniarukenia <mrchildrenkh1008@gmail.com>
2020-02-12 23:35:21 +0900

    スケッチ

--- perl/a.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-function func ()
-{
-  echo -e "filename = ${1}"
-}
-export -f func
-
-find ./ -type f -name "*.pl" | xargs -t -I{} bash -c "func {}"
--- perl/シャッフル.pl
+++
@@ -1,7 +0,0 @@
-#!/usr/local/bin/perl
-
-use List::Util;
-
-my @data = (a..z);
-print join(",",@data),"\n";
-print join(",",List::Util::shuffle @data),"\n";
