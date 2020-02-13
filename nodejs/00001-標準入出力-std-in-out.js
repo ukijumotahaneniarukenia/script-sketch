cc2ba1c3c75625e6599d2ef3102e39d172490176
ukijumotahaneniarukenia <mrchildrenkh1008@gmail.com>
2020-02-14 00:06:30 +0900

    sketch

--- nodejs/00001-標準入出力-std-in-out.js
+++ nodejs/00001-標準入出力-std-in-out.js
@@ -1,7 +1,17 @@
 #!/usr/bin/env node
 
+const usage = async (status) =>{
+    console.log("please stdin via pipe only.");
+    console.log("Usage:");
+    process.exit(status);
+}
+
 const main = async (status) =>{
-    process.exit(status)
+    if(process.argv[2]){
+        usage(1);
+    }
+    process.exit(status);
+
 };
 
 main(0);
