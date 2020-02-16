1e07350b804e7f594bd4bc732ab9f5618ece2b2f
ukijumotahaneniarukenia <mrchildrenkh1008@gmail.com>
2020-02-16 12:21:27 +0900

    repo

--- jq/パス式列挙.sh
+++ jq/パス式列挙.sh
@@ -7,5 +7,6 @@ TGT_FILE=$1;
 cat "$TGT_FILE" | jq -r 'paths|map(if type=="number" then "["+tostring+"]" else "."+tojson end)|join("")' | \
 while read tgt;do
   PATH_EXPR=$(echo "."$tgt) #パス式の先頭にドットを付与
-  cat "$TGT_FILE" | jq -c "$PATH_EXPR" | grep -vP '^{' #オブジェクト以外を出力
-done | xargs -n2
+  cat "$TGT_FILE" | jq -c "$PATH_EXPR" | grep -vP '^{' | \
+    jq -c 'if(test("^items")) then split(".") else . end' | perl -pe 's;(?<="),(?=");\n;g' | perl -pe 's;^\[|\]$;;'
+done
