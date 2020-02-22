55bcdc6cac716b2e2aff0ff1eec58dd9d67477c1
ukijumotahaneniarukenia <mrchildrenkh1008@gmail.com>
2020-02-23 00:47:56 +0900

    sketch

--- java/標準入力.java
+++ java/標準入力.java
@@ -12,10 +12,10 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public class 標準入力 {
-    public static void main(String[] rsv_args) {
-        trap(new String[]{"INT"});
+    public static void main(String[] cmdline_args) {
+        trap(new ArrayList<>(Arrays.asList("INT")));
         try{
-            if(rsv_args.length==0){
+            if(cmdline_args.length==0){
                 //via pipe args
                 Scanner stdin=new Scanner(System.in);
                 List<String> liz = new ArrayList<>();
@@ -27,7 +27,7 @@ public class 標準入力 {
                 stdin.close();
             }else{
                 //via cmdline args
-                List<String> liz = new ArrayList<>(Arrays.asList(rsv_args));
+                List<String> liz = new ArrayList<>(Arrays.asList(cmdline_args));
                 if(liz.size()==1){
                     XXX(new ArrayList<>(Arrays.asList(liz.get(0).split("\n"))));
                 }else{
@@ -36,32 +36,30 @@ public class 標準入力 {
             }
         }catch(Exception e){
             e.printStackTrace();
-            System.exit(0);
+            System.exit(1);
         }
     }
-    private static void XXX(List<String> rsv_liz) throws IOException{
-        for (int i=0;i<rsv_liz.size();i++){
-            List<String> liz = new ArrayList<>(Arrays.asList(rsv_liz.get(i).split(" ")));
-            for(int j=0;j<liz.size();j++){
-                File file = new File(liz.get(j));
+    private static void XXX(List<String> liz) throws IOException{
+        for (int i=0;i<liz.size();i++){
+            List<String> lizz = new ArrayList<>(Arrays.asList(liz.get(i).split(" ")));
+            for(int j=0;j<lizz.size();j++){
+                File file = new File(lizz.get(j));
                 if(file.exists()){
-                    System.out.println("file dayo-");
-                    Path path = Paths.get(liz.get(j));
-                    String txt = Files.readString(path);
-                    YYY(txt);
+                    Path path = Paths.get(lizz.get(j));
+                    String str = Files.readString(path);
+                    YYY(str);
                 }else{
-                    System.out.println("file janaiyo-");
-                    YYY(liz.get(j));
+                    YYY(lizz.get(j));
                 }
             }
         }
     }
-    private static void YYY(String tgt){
-        System.out.print(tgt);
+    private static void YYY(String str){
+        System.out.println(str);
     }
-    private static void trap(String[] sig_args){
-        for (String sig:sig_args) {
-            catch_sig(sig);
+    private static void trap(List<String> liz){
+        for (String ele:liz) {
+            catch_sig(ele);
         }
     }
     private static void usage() {
@@ -74,10 +72,10 @@ public class 標準入力 {
         );
         System.exit(0);
     }
-    private static void catch_sig(String sig) {
-        Signal signal = new Signal(sig);
-        Signal.handle(signal, Signal -> {
-            if ("INT".equals(signal.getName())) {
+    private static void catch_sig(String str) {
+        Signal sig = new Signal(str);
+        Signal.handle(sig, Signal -> {
+            if ("INT".equals(sig.getName())) {
                 usage();
             }
         });
