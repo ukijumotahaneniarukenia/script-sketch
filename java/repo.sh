7050bca106a233e8a1223e6894120ab2d6897d9c
ukijumotahaneniarukenia <mrchildrenkh1008@gmail.com>
2020-02-23 05:13:44 +0900

    sketch

--- java/repo.sh
+++ java/repo.sh
@@ -4,3 +4,7 @@ cd $HOME/script-sketch/java/lib
 
 ls sqlite-jdbc-3.30.1.jar || curl -LO https://bitbucket.org/xerial/sqlite-jdbc/downloads/sqlite-jdbc-3.30.1.jar
 ls sqlite-jdbc-3.30.1.jar && :
+
+
+ls htmlcleaner-2.23.jar || curl -LO https://sourceforge.net/projects/htmlcleaner/files/htmlcleaner/htmlcleaner%20v2.23/htmlcleaner-2.23.zip && unzip htmlcleaner-2.23.zip
+ls htmlcleaner-2.23.jar && :
