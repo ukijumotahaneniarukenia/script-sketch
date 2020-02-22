ee75f7362e425bbb5a012765664fae344ba04341
ukijumotahaneniarukenia <mrchildrenkh1008@gmail.com>
2020-02-22 21:24:52 +0900

    sketch

--- java/java_標準入力の文字コードを推測する_detect_enc_DONE_dctenc.java
+++ java/java_標準入力の文字コードを推測する_detect_enc_DONE_dctenc.java
@@ -1,6 +1,11 @@
 import java.io.File;
 
-import com.ibm.icu.text.Transliterator;
+import com.ibm.icu.text.CharsetDetector;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
 import sun.misc.Signal;
 
 import java.io.IOException;
@@ -12,7 +17,7 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 
-public class java_全角文字から半角文字へ変換_FULL2HALF_DONE_full2half {
+public class java_標準入力の文字コードを推測する_detect_enc_DONE_dctenc {
     public static void main(String[] rsv_args) {
         trap(new String[]{"INT"});
         try{
@@ -36,18 +41,18 @@ public class java_全角文字から半角文字へ変換_FULL2HALF_DONE_full2ha
     }
     private static void process(List<String> tgt_liz) throws IOException {
         for(int i=0;i<tgt_liz.size();i++){
-            System.out.println(full2half(tgt_liz.get(i)));
+            System.out.println(dctenc(tgt_liz.get(i)));
         }
     }
-    private static String full2half(String tgt) throws IOException {
-        File file = new File(tgt);
-        if(!file.exists()){
-          return Transliterator.getInstance("Fullwidth-Halfwidth").transliterate(tgt);
-        }else{
-          Path path = Paths.get(tgt);
-          String txt = Files.readString(path);
-          return Transliterator.getInstance("Fullwidth-Halfwidth").transliterate(txt);
+    private static String dctenc(String text) {
+        InputStream inputStream = new ByteArrayInputStream(text.getBytes());
+        CharsetDetector detector = new CharsetDetector();
+        try {
+            detector.setText(inputStream);
+        } catch (IOException e) {
+            e.printStackTrace();
         }
+        return detector.detect().getName();
     }
     private static void trap(String[] sig_args){
         for (String sig:sig_args) {
