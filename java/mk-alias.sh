9ff1bfbf22be3450638660ec4b86fd70bad47985
ukijumotahaneniarukenia <mrchildrenkh1008@gmail.com>
2020-02-22 20:47:48 +0900

    sketch

--- java/mk-alias.sh
+++ java/mk-alias.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
-rm -rf $HOME/script-sketch/java/out/*
-
 mkdir -p $HOME/script-sketch/java/out
 
-ls $HOME/script-sketch/java | grep DONE | xargs -I@ echo javac -d $HOME/script-sketch/java/out -classpath "$HOME/script-sketch/java/lib/*" @
+rm -rf $HOME/script-sketch/java/out/*
 
+ls $HOME/script-sketch/java | grep DONE | xargs -I@ echo javac -d $HOME/script-sketch/java/out -classpath \"$HOME/script-sketch/java/lib/*\" @ | sh 2>/dev/null
 
 while read tgt;do
 
-  printf "echo \x27alias %s=\x22java -classpath \$HOME/script-sketch/java/out:\$HOME/script-sketch/java/lib/* %s\x22\x27 \n"
+  printf "echo \x27alias %s=\x22java -classpath \$HOME/script-sketch/java/out:\$HOME/script-sketch/java/lib/* %s\x22\x27 >> \$HOME/.bashrc \n" $(echo $tgt | perl -pe 's;.*_;;g' | perl -pe 's;\.class;;g') $(echo $tgt | perl -pe 's;\.class;;g') | sh
 
-done < <(ls $HOME/script-sketch/java/out/*)
+done < <(ls $HOME/script-sketch/java/out)
