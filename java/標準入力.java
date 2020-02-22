792e6dd8606c573409aec8fc9c86311547655ebf
ukijumotahaneniarukenia <mrchildrenkh1008@gmail.com>
2020-02-23 00:28:29 +0900

    sketch

--- java/標準入力.java
+++ java/標準入力.java
@@ -1,6 +1,15 @@
+import java.io.IOException;
+
 import sun.misc.Signal;
 
-import java.util.*;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class 標準入力 {
     public static void main(String[] rsv_args) {
@@ -9,24 +18,46 @@ public class 標準入力 {
             if(rsv_args.length==0){
                 //via pipe args
                 Scanner stdin=new Scanner(System.in);
-                List<String> liz = new ArrayList<>(Arrays.asList(stdin.nextLine().split(" ")));
-                circle(liz);
+                List<String> liz = new ArrayList<>();
+                while (stdin.hasNextLine()) {
+                    liz.add(stdin.nextLine());
+                    XXX(liz);
+                    liz.remove(0);
+                }
                 stdin.close();
             }else{
                 //via cmdline args
                 List<String> liz = new ArrayList<>(Arrays.asList(rsv_args));
-                circle(liz);
+                if(liz.size()==1){
+                    XXX(new ArrayList<>(Arrays.asList(liz.get(0).split("\n"))));
+                }else{
+                    XXX(liz);
+                }
             }
         }catch(Exception e){
             e.printStackTrace();
             System.exit(0);
         }
     }
-    private static void circle(List<String> rsv_liz){
-        for (int ind=0;ind<rsv_liz.size();ind++){
-            Collections.rotate(rsv_liz, 1);
-            System.out.println(rsv_liz);
+    private static void XXX(List<String> rsv_liz) throws IOException{
+        for (int i=0;i<rsv_liz.size();i++){
+            List<String> liz = new ArrayList<>(Arrays.asList(rsv_liz.get(i).split(" ")));
+            for(int j=0;j<liz.size();j++){
+                File file = new File(liz.get(j));
+                if(file.exists()){
+                    System.out.println("file dayo-");
+                    Path path = Paths.get(liz.get(j));
+                    String txt = Files.readString(path);
+                    YYY(txt);
+                }else{
+                    System.out.println("file janaiyo-");
+                    YYY(liz.get(j));
+                }
+            }
         }
+    }
+    private static void YYY(String tgt){
+        System.out.print(tgt);
     }
     private static void trap(String[] sig_args){
         for (String sig:sig_args) {
