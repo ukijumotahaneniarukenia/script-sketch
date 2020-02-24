8eae3dc9c2bba74a8d8a63264536f38dc6092420
ukijumotahaneniarukenia <mrchildrenkh1008@gmail.com>
2020-02-25 00:01:07 +0900

    sketch

--- nim/stdin_via_pipe.nim
+++ nim/stdin_via_pipe.nim
@@ -1,1 +1,14 @@
-let s = readLine(stdin)
+import os
+
+proc main(args: seq[string]): int =
+  if args.len < 1:
+    stdout.writeLine("unko")
+    return 1
+  else:
+    stdout.writeLine("unkojanai")
+    return 0
+
+when isMainModule:
+  let args = commandLineParams()
+  stdout.writeLine(args)
+  quit main(args)
