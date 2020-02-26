2bf72733ba5597359e646ce1a3e2ad1c21140e22
ukijumotahaneniarukenia <mrchildrenkh1008@gmail.com>
2020-02-26 23:10:11 +0900

    sketch

--- ruby/00001-ruby-標準入力の読み込み-stdin-XXX.rb
+++ ruby/00001-ruby-標準入力の読み込み-stdin-XXX.rb
@@ -1,13 +1,22 @@
 #!/usr/bin/env ruby
 
+def usage
+  puts <<~EOF
+Usage:
+  IN :
+  OUT:
+EOF
+  return 0
+end
+
 if ARGV.length > 0
-    addresses = [ *ARGV ]
-    puts addresses
-    puts "コマンドライン引数"
+  addresses = [ *ARGV ]
+  puts addresses[1]
+  puts "コマンドライン引数"
 elsif FileTest.pipe?(STDIN)
-    addresses = STDIN.readlines.map { |line| line.chomp }
-    puts addresses
-    puts "パイプ経由引数"
+  addresses = STDIN.readlines.map { |line| line.chomp }
+  puts addresses
+  puts "パイプ経由引数"
 else
-    raise "引数ないよ"
+  usage
 end
