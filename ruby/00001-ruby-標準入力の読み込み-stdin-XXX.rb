a5626ba48f2966742ed056e43660a747ab87b2c7
ukijumotahaneniarukenia <mrchildrenkh1008@gmail.com>
2020-02-26 23:25:11 +0900

    sketch

--- ruby/00001-ruby-標準入力の読み込み-stdin-XXX.rb
+++ ruby/00001-ruby-標準入力の読み込み-stdin-XXX.rb
@@ -1,21 +1,23 @@
 #!/usr/bin/env ruby
 
 def usage
+  f = File.basename(__FILE__)
   puts <<~EOF
 Usage:
-  IN :
+  IN :  #{f}
   OUT:
 EOF
   return 0
 end
 
 if ARGV.length > 0
-  addresses = [ *ARGV ]
-  puts addresses[1]
+  cmd_args = [ *ARGV ]
+  stdin = cmd_args[0].split(/\n/)
+  puts stdin
   puts "コマンドライン引数"
 elsif FileTest.pipe?(STDIN)
-  addresses = STDIN.readlines.map { |line| line.chomp }
-  puts addresses
+  pipe_args = STDIN.readlines.map { |line| line.chomp }
+  puts pipe_args[3]
   puts "パイプ経由引数"
 else
   usage
