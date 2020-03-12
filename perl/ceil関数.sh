c09c549bc5e309e9a6a4e5f1d6ea8d2bcb037be3
ukijumotahaneniarukenia <mrchildrenkh1008@gmail.com>
2020-03-13 00:29:31 +0900

    環境構築

--- perl/ceil関数.sh
+++ perl/ceil関数.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
 printf "%s\n" {a..j} | xargs -n8 | perl -nlE '$,=":";$n=$.;@a=split(/ /,$_);foreach my $i (0..$#a) {print $n,@a[$i]}'
+
+
+printf "%s\n" {a..j} | xargs -n6 | perl -nlE '$,=":";@a=(split(/ /, $_));foreach my $i (0..$#a){print $.,$i,$a[$i]}'
