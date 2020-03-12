77534d581b401c3f73d39ebd7a3c2a788a4f1e28
ukijumotahaneniarukenia <mrchildrenkh1008@gmail.com>
2020-03-13 00:12:37 +0900

    環境構築

--- awk/ceil.sh
+++ awk/ceil.sh
@@ -23,3 +23,39 @@ seq 10 | awk '
 #9,2
 #10,2
 
+
+$printf "%s\n" {a..j} | xargs -n3 | awk -v OFS="": '{c=split($0,a," ");for(e in a){print e,a[e]}}'
+1:a
+2:b
+3:c
+1:d
+2:e
+3:f
+1:g
+2:h
+3:i
+1:j
+
+$printf "%s\n" {a..j} | xargs -n3 | awk -v RS="" -v OFS="": '{c=split($0,a," ");for(e in a){print e,a[e]}}'
+4:d
+5:e
+6:f
+7:g
+8:h
+9:i
+10:j
+1:a
+2:b
+3:c
+
+$printf "%s\n" {a..j} | xargs -n3 | awk -v RS="" -v OFS="": '{c=split($0,a," ");for(e in a){print e,a[e]}}' | sort -nk1
+1:a
+2:b
+3:c
+4:d
+5:e
+6:f
+7:g
+8:h
+9:i
+10:j
