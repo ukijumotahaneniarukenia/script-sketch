03d5b47b67ebd83b36c09ab957e00850d7e6e427
ukijumotahaneniarukenia <mrchildrenkh1008@gmail.com>
2020-03-13 22:18:22 +0900

    sketch

---
+++ ruby/00004-ruby-日本語ダミデータ作成.rb
@@ -0,0 +1,32 @@
+#!/usr/bin/env ruby
+
+require 'faker'
+
+Faker::Config.locale=:ja
+
+def usage
+  f = File.basename(__FILE__)
+  puts <<~EOF
+Usage:
+  IN : seq 10 | ./#{f} or ./#{f} 2
+  OUT:
+EOF
+  return 0
+end
+
+def mock(ary)
+  ary.map do|e|
+    puts Faker::Lorem.sentence(word_count:e.to_i)
+  end
+end
+
+if ARGV.length > 0
+  cmd_args = [ *ARGV ]
+  args = cmd_args[0].split(/\n/)
+  mock(args)
+elsif FileTest.pipe?(STDIN)
+  pipe_args = STDIN.readlines.map { |line| line.chomp }
+  mock(pipe_args)
+else
+  usage
+end
