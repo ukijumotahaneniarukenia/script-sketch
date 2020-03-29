044e07f129ef4473f75ae1ad11f170752ae526ac
ukijumotahaneniarukenia <mrchildrenkh1008@gmail.com>
2020-03-29 12:17:28 +0900

    sketch

---
+++ c#/00002-c#-標準入力/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _00002_c__標準入力
+{
+    class Program
+    {
+        public static void Main()
+        {
+            if (! Console.IsInputRedirected) {
+                //標準入力からの入力がない場合は、Usage
+                Usage();
+                return; 
+            }
+
+            String s;
+            while ((s = Console.ReadLine())!=null) {
+                Console.WriteLine(s);
+            }
+        }
+
+        private static void Usage(){
+            String doc =@"
+Usage:
+IN: echo {a..f} | xargs -n3 | dotnet run
+OUT:
+    a b c
+    d e f
+";
+            Console.WriteLine(doc);
+        }
+    }
+}
