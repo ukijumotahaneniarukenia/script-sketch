c96e26f8b8e38cdc2f8e17713fb68ecfd1042590
ukijumotahaneniarukenia <mrchildrenkh1008@gmail.com>
2020-03-29 15:44:58 +0900

    sketch

---
+++ c#/00007-c#-実行ファイル名の取得/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Diagnostics;
+using System.Text.RegularExpressions;
+
+namespace _00007_c__実行ファイル名の取得
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String path = "";
+            path = Process.GetCurrentProcess().MainModule.FileName;
+            Console.WriteLine(path);
+            path = Regex.Replace(path, ".*/","");
+            Console.WriteLine(path);
+        }
+    }
+}
