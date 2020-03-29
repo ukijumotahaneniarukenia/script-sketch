0fd2df3cfb57e29c1c59f157b3c15495e2f2cde8
ukijumotahaneniarukenia <mrchildrenkh1008@gmail.com>
2020-03-29 15:23:49 +0900

    sketch

--- c#/00002-c#-パイプ経由引数/Program.cs
+++ c#/00002-c#-パイプ経由引数/Program.cs
@@ -1,11 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 
-namespace _00002_c__標準入力
+namespace _00002_c__パイプ経由引数
 {
     class Program
     {
-        public static void Main()
+        static void Main(string[] args)
         {
             if (! Console.IsInputRedirected) 
             {
@@ -20,7 +20,6 @@ namespace _00002_c__標準入力
             while ((s = Console.ReadLine())!=null) 
             {
                 List<String> liz = new List<String>();
-                n++;
                 int cnt=s.Split(" ").Length;
                 String[] ss=s.Split(" ");
                 for (int i=0;i<=cnt-1;i++)
@@ -28,6 +27,7 @@ namespace _00002_c__標準入力
                     liz.Add(ss[i]);
                 }
                 map.Add(n,liz);
+                n++;
             }
             debug(map);
         }
@@ -50,23 +50,23 @@ namespace _00002_c__標準入力
 Usage:
 IN: echo {a..f} | xargs -n2 | dotnet run
 OUT:
-    1:0:a
-    1:1:b
-    2:0:c
-    2:1:d
-    3:0:e
-    3:1:f
+    0:0:a
+    0:1:b
+    1:0:c
+    1:1:d
+    2:0:e
+    2:1:f
 
 or
 
 IN: echo {a..f} | xargs -n1 | dotnet run
 OUT:
-    1:0:a
-    2:0:b
-    3:0:c
-    4:0:d
-    5:0:e
-    6:0:f
+    0:0:a
+    1:0:b
+    2:0:c
+    3:0:d
+    4:0:e
+    5:0:f
 
 ";
             Console.WriteLine(doc);
