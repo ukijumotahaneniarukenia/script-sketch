1b195aeed29db6e3dcf9a48c643b566de720b9e5
ukijumotahaneniarukenia <mrchildrenkh1008@gmail.com>
2020-03-29 15:14:39 +0900

    sketch

---
+++ c#/00004-c#-reduce/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace _00004_c__reduce
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var ary = new [] { 1, 2, 3, 4, 5 }; //配列
+            var v = ary.Aggregate((p, x) => p + x);
+            Console.WriteLine(v);
+
+            var liz = new List<int> { 6,7,8,9,10};//リスト
+            var vv = liz.Aggregate((p, x) => p + x);
+            Console.WriteLine(vv);
+        }
+    }
+}
---
+++ c#/00005-c#-range/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace _00005_c__range
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IEnumerable<int> squares = Enumerable.Range(1, 10).Select(x => x);
+            var v = Enumerable.Range(1, 10).Aggregate((p, x) => p + x);
+
+            Console.WriteLine(v);
+
+            foreach (int num in squares)
+            {
+                Console.WriteLine(num);
+            }
+        }
+    }
+}
