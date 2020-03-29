4770b5ba18950854e3286adffab45f33487e2a8e
ukijumotahaneniarukenia <mrchildrenkh1008@gmail.com>
2020-03-29 23:45:43 +0900

    sketch

--- nim/00008-nim-cycle/main.nim
+++ nim/00008-nim-cycle/main.nim
@@ -1,9 +1,9 @@
 import os
 import strutils
 import sequtils
+import tables
 
 proc usage() =
-  #シグナルキャッチできたらよびたい
   let usage = """
 Usage:
 IN:
@@ -11,19 +11,38 @@ OUT:
 """
   echo usage
 
+proc debug(t: Table[int, seq[string]]) =
+  for k,v in t.pairs:
+    echo k,v
+
+proc my_cycle(t: Table[int, seq[string]]) =
+  var rt : seq[string]
+  for k,v in t.pairs:
+    rt = v[(low(v)+1)..high(v)].cycle(v[0].parseInt)
+    echo rt
+
 proc main(args: seq[string]): int =
-  var n=0
+  var n = 0
+  var t = initTable[int, seq[string]]()
+  var a : seq[string]
   if 0 == len(args) :
+    #パイプ経由引数の場合
     for line in stdin.lines:
-      n+=1
-      echo line
-      echo n
+      n = n + 1
+      a = line.split(" ")
+      t[n] = a
+    my_cycle(t)
     return 0
-  elif 0<len(args) :
+  elif 0 < len(args) :
+    #コマンドライン引数の場合
     for i in low(args)..high(args):
-      echo args[i]
+      n = n + 1
+      a.add(args[i])
+      t[1] = a
+    my_cycle(t)
     return 0
   else :
+    #起動引数がない場合
     usage()
     return 0
 
