066b42f2fafa0e73407485e6471118fa8bb28c98
ukijumotahaneniarukenia <mrchildrenkh1008@gmail.com>
2020-03-29 13:18:32 +0900

    sketch

--- c#/00002-c#-標準入力/Program.cs
+++ c#/00002-c#-標準入力/Program.cs
@@ -7,7 +7,8 @@ namespace _00002_c__標準入力
     {
         public static void Main()
         {
-            if (! Console.IsInputRedirected) {
+            if (! Console.IsInputRedirected) 
+            {
                 //標準入力からの入力がない場合は、Usage
                 Usage();
                 return; 
@@ -16,33 +17,57 @@ namespace _00002_c__標準入力
             String s;
             int n=0;
             Dictionary<int, List<String>> map = new Dictionary<int, List<String>>();
-            String[,] nnn = new string[0, 0];
-            while ((s = Console.ReadLine())!=null) {
+            while ((s = Console.ReadLine())!=null) 
+            {
                 List<String> liz = new List<String>();
                 n++;
                 int cnt=s.Split(" ").Length;
                 String[] ss=s.Split(" ");
-                for (int i=0;i<=cnt-1;i++){
+                for (int i=0;i<=cnt-1;i++)
+                {
                     liz.Add(ss[i]);
                 }
                 map.Add(n,liz);
             }
+            debug(map);
+        }
+
+        private static void debug(Dictionary<int, List<String>> map )
+        {
             foreach(int k in map.Keys)
             {
                 int cnt = map[k].Count;
-                for (int i=0;i<=cnt-1;i++){
+                for (int i=0;i<=cnt-1;i++)
+                {
                     Console.WriteLine(k+":"+i+":"+map[k][i]);
                 }
             }
         }
 
-        private static void Usage(){
+        private static void Usage()
+        {
             String doc =@"
 Usage:
-IN: echo {a..f} | xargs -n3 | dotnet run
+IN: echo {a..f} | xargs -n2 | dotnet run
 OUT:
-    a b c
-    d e f
+    1:0:a
+    1:1:b
+    2:0:c
+    2:1:d
+    3:0:e
+    3:1:f
+
+or
+
+IN: echo {a..f} | xargs -n1 | dotnet run
+OUT:
+    1:0:a
+    2:0:b
+    3:0:c
+    4:0:d
+    5:0:e
+    6:0:f
+
 ";
             Console.WriteLine(doc);
         }
