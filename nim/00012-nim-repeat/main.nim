0a8e3e6ebb0f8809baa49f32666717e5c25831b4
ukijumotahaneniarukenia <mrchildrenkh1008@gmail.com>
2020-03-29 23:55:11 +0900

    sketch

---
+++ nim/00012-nim-repeat/main.nim
@@ -0,0 +1,51 @@
+import os
+import strutils
+import sequtils
+import tables
+
+proc usage() =
+  let usage = """
+Usage:
+IN:
+OUT:
+"""
+  echo usage
+
+proc debug(t: Table[int, seq[string]]) =
+  for k,v in t.pairs:
+    echo k,v
+
+proc my_repeat(t: Table[int, seq[string]]) =
+  var rt : string
+  for k,v in t.pairs:
+    rt = repeat(v[0],v[1].parseInt)
+    echo rt
+
+proc main(args: seq[string]): int =
+  var n = 0
+  var t = initTable[int, seq[string]]()
+  var a : seq[string]
+  if 0 == len(args) :
+    #パイプ経由引数の場合
+    for line in stdin.lines:
+      n = n + 1
+      a = line.split(" ")
+      t[n] = a
+    my_repeat(t)
+    return 0
+  elif 0 < len(args) :
+    #コマンドライン引数の場合
+    for i in low(args)..high(args):
+      n = n + 1
+      a.add(args[i])
+      t[1] = a
+    my_repeat(t)
+    return 0
+  else :
+    #起動引数がない場合
+    usage()
+    return 0
+
+when isMainModule:
+  let args = commandLineParams()
+  quit main(args)
