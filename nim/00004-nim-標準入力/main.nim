2ffbe15188b569dd94b00bbc47b85a9cb45f5031
ukijumotahaneniarukenia <mrchildrenkh1008@gmail.com>
2020-03-29 20:13:43 +0900

    sketch

--- nim/00004-nim-標準入力/main.nim
+++ nim/00004-nim-標準入力/main.nim
@@ -2,15 +2,26 @@ import os
 import strutils
 import sequtils
 
+proc usage() =
+  #シグナルキャッチできたらよびたい
+  let usage = """
+Usage:
+IN:
+OUT:
+"""
+  echo usage
+
 proc main(args: seq[string]): int =
-  if args.len == 0:
+  if 0 == len(args) :
     for line in stdin.lines:
       echo line
     return 0
-  else:
-    stdout.writeLine(args[0])
-    stdout.writeLine(args[1])
-    stdout.writeLine(args[2])
+  elif 0<len(args) :
+    for i in low(args)..high(args):
+      echo args[i]
+    return 0
+  else :
+    usage()
     return 0
 
 when isMainModule:
