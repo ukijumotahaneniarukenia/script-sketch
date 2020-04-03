0ae83a86cbc539a2a153ebccdef15a8b43702058
ukijumotahaneniarukenia <mrchildrenkh1008@gmail.com>
2020-04-03 22:18:40 +0900

    sketch

--- c/00001-c-標準入力/main.c
+++ c/00001-c-標準入力/main.c
@@ -1,18 +1,19 @@
 #include <stdio.h>
-#include <unistd.h>
 
-int main() {
+int main(int argc,char **argv) {
 
-    const int BUF_LEN = 255;
-    char buf[BUF_LEN];
+  FILE *rf;
+  const int BUF_LEN = 4096;
+  char buf[BUF_LEN];
 
-    int rc;
-    rc = read(STDIN_FILENO, buf, BUF_LEN);
-    if (rc == -1) {
-        return 1;
-    }
+  printf("%s\n",*argv);
+  printf("%s\n",argv[0]);
+  printf("%s\n",argv[1]);
 
-    fwrite(buf, 1, rc, stdout);
+  rf=fopen(argv[1],"r");//ファイルを読込専用でオープン
+  fread(buf, sizeof(int), 10, rf);//ファイルから読み込み,バッファーへ格納
+  fclose(rf);//読込専用でオープンしたファイルをクローズ
+  fwrite(buf, 10, 1, stdout);//バッファーから読み込み,標準出力へ出力
 
-    return 0;
+  return 0;
 }
