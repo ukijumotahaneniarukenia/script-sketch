4822f13c03b28a137cab92a5eeb8bda952d314ba
ukijumotahaneniarukenia <mrchildrenkh1008@gmail.com>
2020-04-03 18:05:07 +0900

    sketch

--- python/20001-sqlite3とpython3を使って辞書構築.py
+++ python/20001-sqlite3とpython3を使って辞書構築.py
@@ -1,53 +1,44 @@
+#!/usr/bin/env python3
+
 import sqlite3
 import sys
-from os import path
+import os
 
-#TODOヒアドキュメントで変数を使用したい
 def usage():
-  usage='''
+  filename=__file__
+  usage="""
 Usage:
-  $python3 20001-sqlite3とpython3を使って辞書構築.py pages.db 20001-sqlite3とpython3を使って辞書構築.sql NTT_DoCoMo
-  $echo pages.db 20001-sqlite3とpython3を使って辞書構築.sql NTT_DoCoMo | python3 20001-sqlite3とpython3を使って辞書構築.py
-'''
+IN:  echo pages.db 20001-sqlite3とpython3を使って辞書構築.sql NTT_DoCoMo | {filename}
+OUT:
+""".format(filename=filename)
+
   print(usage)
   sys.exit(0)
 
-def read_sql_text(sql_file_path):
-  with open('./'+sql_file_path) as f:
+def XXX(args):
+  tgt_db=args[0]
+  script_filepath=args[1]
+  search_word=args[2]
+  conn=sqlite3.connect(tgt_db)
+  with open(script_filepath) as f:
     sql_txt=f.read()
-    return sql_txt
-
-def create_conn(tgt_db):
-  return (sqlite3.connect(tgt_db))
-
-def create_dict(conn,sql_txt):
-  return (dict(conn.execute(sql_txt).fetchall()))
-
-def select_tgt(conn,tgt_dict,tgt_str):
-  return [(conn),(tgt_dict[tgt_str])]
-
-def destroy_conn(conn):
+  dict_tbl=dict(conn.execute(sql_txt).fetchall())
+  return dict_tbl[search_word]
   conn.close()
 
-#TODO複数の検索ワードに対応したい
 def main():
   rt=""
   try:
     if (len(sys.argv[1:]))==0:
       #パイプ経由引数の場合
-      #改行を取り除く
-      stdin=list(map(lambda x:x.strip(),sys.stdin.readlines()))[0].split()
-      rt=select_tgt(create_conn(stdin[0]),create_dict(create_conn(stdin[0]),read_sql_text(stdin[1])),stdin[2])
+      args=list(map(lambda x:x.strip(),sys.stdin.readlines()))[0].split()
+      rt=XXX(args)
+      print(rt)
     else:
       #コマンドライン経由引数の場合
-      #先頭要素のファイル名を除く
-      stdin=sys.argv[1:]
-      rt=select_tgt(create_conn(stdin[0]),create_dict(create_conn(stdin[0]),read_sql_text(stdin[1])),stdin[2])
+      usage()
   except KeyboardInterrupt:
     usage()
-  else:
-    return rt[1]
-  finally:
-    destroy_conn(rt[0])
 
-print(main())
+if __name__=="__main__":
+  main()
