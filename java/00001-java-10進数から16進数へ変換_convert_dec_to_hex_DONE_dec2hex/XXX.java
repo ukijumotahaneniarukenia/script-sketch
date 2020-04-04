c18864b91bcf7330256d0b095fa1c4950f4abe20
ukijumotahaneniarukenia <mrchildrenkh1008@gmail.com>
2020-04-04 22:39:06 +0900

    sketch

--- java/00001-java-10進数から16進数へ変換_convert_dec_to_hex_DONE_dec2hex/XXX.java
+++ java/00001-java-10進数から16進数へ変換_convert_dec_to_hex_DONE_dec2hex/XXX.java
@@ -6,24 +6,24 @@ import java.util.HashMap;
 import sun.misc.Signal;
 
 public class XXX {
-    public static void main(String[] cmdline_args) {
-        HashMap<Integer, List<String>> maz=new HashMap<Integer, List<String>>();
-        maz=KKK(cmdline_args,new Scanner(System.in),Arrays.asList("INT"));
-        debug_maz(maz);
-    }
-    public static HashMap<Integer, List<String>> KKK(String[] cmdline_args,Scanner stdin,List<String> signal_args) {
+    //public static void main(String[] cmdLineArgs) {
+    //    HashMap<Integer, List<String>> maz=new HashMap<Integer, List<String>>();
+    //    maz=KKK(cmdLineArgs,new Scanner(System.in),Arrays.asList("INT"));
+    //    debug_maz(maz);
+    //}
+    public static HashMap<Integer, List<String>> KKK(String className,String cmdInput,String[] cmdLineArgs,Scanner stdin,List<String> signalArgs) {
         List<String> liz=new ArrayList<>();
         HashMap<Integer, List<String>> maz=new HashMap<Integer, List<String>>();
-        trap(new ArrayList<>(signal_args));
+        trap(className,cmdInput,new ArrayList<>(signalArgs));
         try{
-            if(cmdline_args.length==0){
+            if(cmdLineArgs.length==0){
                 //via pipe args
                 liz=pre_process(stdin);
                 maz=sub_process(liz);
                 //debug_maz(maz);
             }else{
                 //via cmdline args
-                liz = new ArrayList<>(Arrays.asList(cmdline_args));
+                liz = new ArrayList<>(Arrays.asList(cmdLineArgs));
                 if(liz.size()==1){
                     maz=sub_process(new ArrayList<>(Arrays.asList(liz.get(0).split("\n"))));
                     //debug_maz(maz);
@@ -64,20 +64,24 @@ public class XXX {
         }
         return maz;
     }
-    private static void trap(List<String> liz){
+    private static void trap(String className,String cmdInput,List<String> liz){
         for (String ele:liz) {
             Signal sig = new Signal(ele);
             Signal.handle(sig, Signal -> {
                 if (ele.equals(sig.getName())) {
-                    usage();
+                    usage(className,cmdInput);
                 }
             });
         }
     }
-    private static void usage() {
-        final String className = new Object(){}.getClass().getEnclosingClass().getName();
+    private static void usage(String className,String cmdInput) {
+        if(cmdInput.isEmpty()){
+            System.out.println("Usageを完成させてから実行してね");
+            System.out.println(cmdInput.isEmpty() ? "INPUT" : "Usage:"+ cmdInput + "java" + className.replaceAll(".*_", "") + "\n");
+            System.exit(1);
+        }
         System.out.println("\nUsage:\n" +
-          "seq 17 | " + className.replaceAll(".*_", "") + "\n"
+          cmdInput + " java " + className.replaceAll(".*_", "") + "\n"
         );
         System.exit(0);
     }
--- java/00001-java-10進数から16進数へ変換_convert_dec_to_hex_DONE_dec2hex/dec2hex.java
+++ java/00001-java-10進数から16進数へ変換_convert_dec_to_hex_DONE_dec2hex/dec2hex.java
@@ -1,79 +1,31 @@
 import java.io.IOException;
-import java.io.File;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-
+import java.util.HashMap;
 import sun.misc.Signal;
 
 public class dec2hex {
+    public static final String className = new Object(){}.getClass().getEnclosingClass().getName();
+    public static final String cmdInput="seq 17 |";
     public static void main(String[] cmdline_args) {
-        trap(new ArrayList<>(Arrays.asList("INT")));
-        try{
-            if(cmdline_args.length==0){
-                //via pipe args
-                Scanner stdin=new Scanner(System.in);
-                List<String> liz = new ArrayList<>();
-                while (stdin.hasNextLine()) {
-                    liz.add(stdin.nextLine());
-                    XXX(liz);
-                    liz.remove(0);
-                }
-                stdin.close();
-            }else{
-                //via cmdline args
-                List<String> liz = new ArrayList<>(Arrays.asList(cmdline_args));
-                if(liz.size()==1){
-                    XXX(new ArrayList<>(Arrays.asList(liz.get(0).split("\n"))));
-                }else{
-                    XXX(liz);
-                }
-            }
-        }catch(Exception e){
-            e.printStackTrace();
+        XXX xxx = new XXX();
+        HashMap<Integer, List<String>> maz=new HashMap<Integer, List<String>>();
+        maz=xxx.KKK(className,cmdInput,cmdline_args,new Scanner(System.in),Arrays.asList("INT"));
+        dec2hex(maz);
+    }
+    private static void dec2hex(HashMap<Integer, List<String>> maz){
+        maz.forEach((k,v)->{
+          if(1 != v.size()){
+            System.out.println("#1. java "+className+"\n"
+                + "#2. Enter" + "\n"
+                + "#3. Ctrl+C" + "\n"
+            );
             System.exit(1);
-        }
-    }
-    private static void XXX(List<String> liz) throws IOException{
-        for (int i=0;i<liz.size();i++){
-            List<String> lizz = new ArrayList<>(Arrays.asList(liz.get(i).split(" ")));
-            for(int j=0;j<lizz.size();j++){
-                File file = new File(lizz.get(j));
-                if(file.exists()){
-                    Path path = Paths.get(lizz.get(j));
-                    String str = Files.readString(path);
-                    dec2hex(str);
-                }else{
-                    dec2hex(lizz.get(j));
-                }
-            }
-        }
-    }
-    private static void trap(List<String> liz){
-        for (String ele:liz) {
-            catch_sig(ele);
-        }
-    }
-    private static void usage() {
-        final String className = new Object(){}.getClass().getEnclosingClass().getName();
-        System.out.println("\nUsage:\n" +
-        "seq 17 | " + className.replaceAll(".*_", "") + "\n"
-        );
-        System.exit(0);
-    }
-    private static void catch_sig(String str) {
-        Signal sig = new Signal(str);
-        Signal.handle(sig, Signal -> {
-            if ("INT".equals(sig.getName())) {
-                usage();
-            }
+          }else{
+            System.out.println(Integer.toHexString(Integer.valueOf(v.get(0))));
+          }
         });
-    }
-    private static void dec2hex(String str) {
-        System.out.println(Integer.toHexString(Integer.valueOf(str)));
     }
 }
