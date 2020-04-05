df251896b31d1e3b48450c9ca7c51940fb36725a
ukijumotahaneniarukenia <mrchildrenkh1008@gmail.com>
2020-04-05 15:05:59 +0900

    sketch

--- java/00007-java-クローラー-crawling-DONE-crawl/nnn/src/main/java/app/App.java
+++ java/00007-java-クローラー-crawling-DONE-crawl/nnn/src/main/java/app/App.java
@@ -1,13 +1,36 @@
 package app;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Scanner;
+
+public class App {
+    public static final String className = new Object(){}.getClass().getEnclosingClass().getName();
+    public static final String cmdInput="seq 17 |";
+    public static void main(String[] cmdline_args) {
+        XXX xxx = new XXX();
+        HashMap<Integer, List<String>> maz;
+        maz=xxx.KKK(className,cmdInput,cmdline_args,new Scanner(System.in),Arrays.asList("INT"));
+        crawl(maz);
+    }
+    private static void crawl(HashMap<Integer, List<String>> maz){
+        maz.forEach((k,v)->{
+            System.out.printf("%s:%s\n",k,v);
+//            try {
+//                Document doc = Jsoup.connect(v.get(0)).get();
+//                System.out.printf("%s\n",doc.title());
+//                Elements newsHeadlines = doc.select(v.get(1));
+//                for (Element headline : newsHeadlines) {
+//                    System.out.printf("%s\t%s\n",headline.attr("title"), headline.absUrl("href"));
+//                }
+//            } catch (IOException e) {
+//                e.printStackTrace();
+//            }
+        });
     }
 }
