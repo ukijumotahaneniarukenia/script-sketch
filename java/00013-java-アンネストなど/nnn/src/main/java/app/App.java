c652dc78ed6498d041d7ff9b7f0fdaee49db9e09
ukijumotahaneniarukenia <mrchildrenkh1008@gmail.com>
2020-04-09 07:49:40 +0900

    sketch

--- java/00013-java-アンネストなど/nnn/src/main/java/app/App.java
+++ java/00013-java-アンネストなど/nnn/src/main/java/app/App.java
@@ -1,28 +1,56 @@
 package app;
 
 import java.util.*;
+import java.util.concurrent.atomic.AtomicReference;
 import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 
 public class App {
-    public static void main( String[] args ) {
-        Map<String, List<Integer>> m = new HashMap<String, List<Integer>>(){
-            {
-                put("apple", niz(3,5));
-                put("banana", niz(2,3));
-                put("orange", niz(5,9));
-                put("kiwii", niz(2,6));
-            }
-        };
-        System.out.println(m);
-        System.out.println(unnest(m));
-        List<Map<String, Integer>> l = unnest(m);
-        for(int i=0;i<l.size();i++){
-          System.out.println(l.get(i));
+
+    //それ用の型をもつのが楽なのか
+
+    static Item newItem(String key, int value) {
+        Item o = new Item();
+        o.key = key;
+        o.value = value;
+        return o;
+    }
+
+    static class Item {
+        private String key;
+        private int value;
+        public String key() {
+            return key;
+        }
+        public int value() {
+            return value;
         }
     }
-    private static List<Integer> niz(Integer s,Integer e){
-        return IntStream.range(s,e+1).boxed().collect(Collectors.toList());
+
+    public static void main( String[] args ) {
+        List<Item> l = new ArrayList<>();
+        l.add(newItem("banana",2));
+        l.add(newItem("banana",3));
+        l.add(newItem("orange",5));
+        l.add(newItem("orange",6));
+        l.add(newItem("orange",7));
+        l.add(newItem("orange",8));
+        l.add(newItem("orange",9));
+        l.add(newItem("apple",3));
+        l.add(newItem("apple",4));
+        l.add(newItem("apple",5));
+        l.add(newItem("kiwii",2));
+        l.add(newItem("kiwii",3));
+        l.add(newItem("kiwii",4));
+        l.add(newItem("kiwii",5));
+        l.add(newItem("kiwii",6));
+        System.out.println(l);
+
+        Map<String ,List<Integer>> m = l.stream().collect(Collectors.groupingBy(e->e.key,Collectors.mapping(e->e.value,Collectors.toList())));
+
+        m.entrySet().stream().forEach((Map.Entry<String,List<Integer>> entry) -> System.out.printf("%s:%s\n",entry.getKey(),entry.getValue()));
+
+        unnest(m).stream().forEach(stringIntegerMap -> System.out.println(stringIntegerMap));
+
     }
     private static List<Map<String, Integer>> unnest (Map<String, List<Integer>> m){
         List<Map<String, Integer>> rt = new ArrayList<>();
