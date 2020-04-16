c77780dcebd166c3746694af3e5af9a11d50110f
ukijumotahaneniarukenia <mrchildrenkh1008@gmail.com>
2020-04-17 07:31:47 +0900

    sketch

--- java/00042-java-Unicodeテーブル生成/nnn/src/main/java/app/App.java
+++ java/00042-java-Unicodeテーブル生成/nnn/src/main/java/app/App.java
@@ -1,11 +1,8 @@
 package app;
 
 import java.util.*;
-import java.util.function.Function;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
-import java.util.stream.Stream;
 
 //https://github.com/vdurmont/emoji-Java
 //https://www.it-swarm.dev/ja/java/java%E6%96%87%E5%AD%97%E5%88%97%E3%81%8B%E3%82%89%E2%9C%85%E3%80%81%E3%80%81%E2%9C%88%E3%80%81%E2%99%9B%E3%81%AA%E3%81%A9%E3%81%AE%E7%B5%B5%E6%96%87%E5%AD%97-imagessign%E3%82%92%E5%89%8A%E9%99%A4%E3%81%97%E3%81%BE%E3%81%99%E3%80%82/837301366/
@@ -36,49 +33,35 @@ public class App {
     private static List<String> lookUp(Map<Integer,List<String>> m,List<Integer> l,Integer...n){
         return l.stream().map(e->m.get(e).get(0!=n.length&&m.get(e).size()>n[0]?n[0]:0)).collect(Collectors.toList());
     }
-
-    //https://stackoverflow.com/questions/22625065/collectors-groupingby-doesnt-accept-null-keys
-    public static <T, A> Collector<T, ?, Map<A, List<T>>>
-    groupingBy_WithNullKeys(Function<? super T, ? extends A> classifier) {
-        return Collectors.toMap(
-                classifier,
-                Collections::singletonList,
-                (List<T> oldList, List<T> newEl) -> {
-                    List<T> newList = new ArrayList<>(oldList.size() + 1);
-                    newList.addAll(oldList);
-                    newList.addAll(newEl);
-                    return newList;
-                });
+    private static Map<Character.UnicodeBlock,List<Integer>> yyy(Map<Integer,Character.UnicodeBlock> m){
+        return m.entrySet().stream().collect(Collectors.groupingBy(e->e.getValue(),Collectors.mapping(e->e.getKey(),Collectors.toList())));
     }
-    private static void yyy(Map<Integer,Character.UnicodeBlock> m){
-
-        System.out.println(m);
-//        System.out.println(m.getOrDefault(10000,m.get(0)));
-//        System.out.println(m.get(10000));
-        Map<Character.UnicodeBlock,List<Integer>> rt = new HashMap<>();
-//        m.entrySet().stream().forEach(e-> System.out.println(e.getKey()));
-//        m.entrySet().stream().forEach(e-> System.out.println(e.getValue()));
-//        rt = m.entrySet().stream().collect(Collectors.groupingBy(e->m.getOrDefault(e.getValue(),m.get(0)),Collectors.mapping(e->e.getKey(),Collectors.toList())));
-//        m.entrySet().stream().collect(groupingBy_WithNullKeys(e->e.getValue(),Collectors.mapping(e->e.getKey(),Collectors.toList())));
-//        rt.entrySet().stream().limit(10).forEach(unicodeBlockListEntry -> System.out.printf("%s\t%s\n",unicodeBlockListEntry.getKey(),unicodeBlockListEntry.getValue().size()));
-    }
-
     public static void main( String[] args ) {
         int s=0;
-        int e=0x333B;
+//        int e=0x333B;
+//        int e=0xFFF;
+//        int e=0xFF;
+        int e=0x10FFFF;
         Map<Integer,List<String>> m = new LinkedHashMap<>();
         Map<Integer,Character.UnicodeBlock> mm = new LinkedHashMap<>();
         List<String> l;
         List<Character.UnicodeBlock> ll;
         for(int i=s;i<=e;i++){
             m.put(i, Arrays.asList(nnn(ppp(i)),ppp(i),qqq(i),sss(i)));
-            mm.put(i, rrr(i));
+            Character.UnicodeBlock rt =rrr(i);
+            if(Objects.nonNull(rt)){
+                //ブロック名が定義されていないものは除外
+                mm.put(i, rt);
+            }
         }
-        l=lookUp(m,IntStream.rangeClosed(0,10).boxed().collect(Collectors.toList()),2);
-        l=lookUp(m,IntStream.rangeClosed(0,10).boxed().collect(Collectors.toList()));
+        l=lookUp(m,IntStream.rangeClosed(2160,2207).boxed().collect(Collectors.toList()),2);
+        l=lookUp(m, IntStream.rangeClosed(2160,2207).boxed().collect(Collectors.toList()));
         ll=lookUp2(mm,IntStream.rangeClosed(0,10).boxed().collect(Collectors.toList()));
-//        System.out.println(ll);
-        yyy(mm);
-//        new Character.UnicodeBlock("ADLM");
+        Map<Character.UnicodeBlock,List<Integer>> rt = yyy(mm);
+        System.out.println(Character.UnicodeBlock.forName("THAANA"));
+        System.out.println(rt.get(Character.UnicodeBlock.forName("THAANA")));
+        l=lookUp(m,rt.get(Character.UnicodeBlock.forName("THAANA")),2);
+        l=lookUp(m,rt.get(Character.UnicodeBlock.forName("THAANA")));
+        System.out.println(l);
     }
 }
