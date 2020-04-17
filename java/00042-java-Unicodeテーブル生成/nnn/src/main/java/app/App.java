942db9304c5c7bfbfa40d7c5ff3341378ae6ab38
ukijumotahaneniarukenia <mrchildrenkh1008@gmail.com>
2020-04-17 22:07:01 +0900

    sketch

--- java/00042-java-Unicodeテーブル生成/nnn/src/main/java/app/App.java
+++ java/00042-java-Unicodeテーブル生成/nnn/src/main/java/app/App.java
@@ -13,28 +13,12 @@ public class App {
     private static String cp2str(Integer n) {
         return new String(Character.toChars(n));
     }
-
-    private static String hex2bin(String s){
-        return Integer.toBinaryString(Integer.parseInt(s,16));
+    private static String cp2uniscript(Integer n){
+        return Character.UnicodeScript.of(n).name();
     }
-
-//    private static void grpUtf8(List<String> l){
-//        Pattern p = Pattern.compile("^1*0");
-//        l.stream().map(e->{
-//            Matcher mc = p.matcher(e);
-//            StringBuilder sb = new StringBuilder();
-//            if(mc.find()){
-//                if(0!=sb.length()){
-//                    sb.append(",");
-//                }
-//                if(2!=mc.group().length()){
-//                    sb.append("\n"+e);
-//                }
-//                sb.append(e);
-//            }
-//            return sb.toString();
-//        }).forEach(e-> System.out.println(e));
-//    }
+    private static String cp2uniname(Integer n){
+        return Character.getName(n);
+    }
     private static String str2uni(String s){
         return IntStream.range(0,s.length()).boxed().map(e->String.format("U+%X",(int)s.charAt(e))).collect(Collectors.joining("-"));
     }
@@ -53,13 +37,13 @@ public class App {
     public static void main( String[] args ) {
 //        int s=0;
 //        int e=0x10FFFF;
-//        int s=0x3040;
-//        int e=0x30FF;
-        int s=0x1F400;
-        int e=0x1F4FF;
+        int s=0x3040;
+        int e=0x30FF;
+//        int s=0x1F400;
+//        int e=0x1F4FF;
         Map<Integer, List<String>> m = new HashMap<>();
         for(int i=s;i<=e;i++){
-            m.put(i, Arrays.asList(String.valueOf(i),cp2str(i),str2utf8(cp2str(i)),str2utf16(cp2str(i)),str2utf32(cp2str(i)),str2uni(cp2str(i))));
+            m.put(i, Arrays.asList(String.valueOf(i),cp2uniname(i),cp2str(i),cp2uniscript(i),str2utf8(cp2str(i)),str2utf16(cp2str(i)),str2utf32(cp2str(i)),str2uni(cp2str(i))));
         }
         for(Map.Entry<Integer, List<String>> entry : m.entrySet()){
             System.out.printf("%s\t%s\n",entry.getKey(),entry.getValue());
