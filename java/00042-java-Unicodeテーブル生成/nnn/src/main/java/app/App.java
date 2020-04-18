b7f5694861b27ba04db79cc61c63f6877793d73e
ukijumotahaneniarukenia <mrchildrenkh1008@gmail.com>
2020-04-18 14:12:17 +0900

    sketch

--- java/00042-java-Unicodeテーブル生成/nnn/src/main/java/app/App.java
+++ java/00042-java-Unicodeテーブル生成/nnn/src/main/java/app/App.java
@@ -3,10 +3,7 @@ package app;
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 import java.text.Normalizer;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
@@ -30,7 +27,7 @@ public class App {
         return Character.getName(n);
     }
     private static String str2uni(String s){
-        return IntStream.range(0,s.length()).boxed().map(e->String.format("U+%X",(int)s.charAt(e))).collect(Collectors.joining("-"));
+        return IntStream.range(0,s.length()).boxed().map(e->String.format("U+%05X",(int)s.charAt(e))).collect(Collectors.joining("-"));
     }
     private static String str2utf8(String s) {
         byte[] b = s.getBytes(StandardCharsets.UTF_8);
@@ -81,40 +78,75 @@ public class App {
     private static String str2norm(String s, Normalizer.Form typ) {
         return Normalizer.normalize(s,typ);
     }
-    public static void main( String[] args ) {
-        int s=0;
-        int e=0x10FFFF;
-//        int s=0;
-//        int e=0xFF;
-//        int s=0x3040;
-//        int e=0x30FF;
-//        int s=0x1F400;
-//        int e=0x1F4FF;
-        Map<Integer, List<String>> m = new HashMap<>();
+    private static Map<Integer, List<String>> mkTbl(Integer s,Integer e){
+        Map<Integer, List<String>> rt = new LinkedHashMap<>();
         for(int i=s;i<=e;i++){
-            m.put(i, Arrays.asList(
+            rt.put(i, Arrays.asList(
                     String.valueOf(i)
                     ,cp2uniname(i)
                     ,cp2str(i)
                     ,cp2uniscript(i)
-                    ,str2utf8(cp2str(i))
-                    ,str2utf16(cp2str(i))
-                    ,str2utf32(cp2str(i))
-                    ,str2uni(cp2str(i))
+//                    ,str2utf8(cp2str(i))//律速ボトルネック
+//                    ,str2utf16(cp2str(i))//律速ボトルネック
+//                    ,str2utf32(cp2str(i))//律速ボトルネック
+//                    ,str2uni(cp2str(i))//律速ボトルネック
                     ,str2norm(cp2str(i), Normalizer.Form.NFC)
-                    ,str2utf8(str2norm(cp2str(i), Normalizer.Form.NFC))
+//                    ,str2utf8(str2norm(cp2str(i), Normalizer.Form.NFC))
+//                    ,str2utf16(str2norm(cp2str(i), Normalizer.Form.NFC))
+//                    ,str2utf32(str2norm(cp2str(i), Normalizer.Form.NFC))
                     ,str2norm(cp2str(i), Normalizer.Form.NFD)
-                    ,str2utf8(str2norm(cp2str(i), Normalizer.Form.NFD))
+//                    ,str2utf8(str2norm(cp2str(i), Normalizer.Form.NFD))
+//                    ,str2utf16(str2norm(cp2str(i), Normalizer.Form.NFD))
+//                    ,str2utf32(str2norm(cp2str(i), Normalizer.Form.NFD))
                     ,str2norm(cp2str(i), Normalizer.Form.NFKC)
-                    ,str2utf8(str2norm(cp2str(i), Normalizer.Form.NFKC))
+//                    ,str2utf8(str2norm(cp2str(i), Normalizer.Form.NFKC))
+//                    ,str2utf16(str2norm(cp2str(i), Normalizer.Form.NFKC))
+//                    ,str2utf32(str2norm(cp2str(i), Normalizer.Form.NFKC))
                     ,str2norm(cp2str(i), Normalizer.Form.NFKD)
-                    ,str2utf8(str2norm(cp2str(i), Normalizer.Form.NFKD))
-                    ,str2utf16(str2norm(cp2str(i), Normalizer.Form.NFKD))
-                    ,str2utf32(str2norm(cp2str(i), Normalizer.Form.NFKD))
+//                    ,str2utf8(str2norm(cp2str(i), Normalizer.Form.NFKD))
+//                    ,str2utf16(str2norm(cp2str(i), Normalizer.Form.NFKD))
+//                    ,str2utf32(str2norm(cp2str(i), Normalizer.Form.NFKD))
             ));
         }
-        for(Map.Entry<Integer, List<String>> entry : m.entrySet()){
-            System.out.printf("%s\t%s\n",entry.getKey(),entry.getValue());
-        }
+        return rt;
+    }
+    private static void printOut(Map<Integer, List<String>> m){
+        m.entrySet().stream().filter(e->e.getValue().get(3).contains(keyWord)).forEach(e-> System.out.println(e));
+    }
+    private static Map<Integer, String> getCol(Map<Integer, List<String>> m,Integer n){
+        return m.entrySet().stream().collect(Collectors.toMap(e->e.getKey(),e->e.getValue().get(n)));
+    }
+
+    private static String keyWord="KATAKANA";
+//    private static String keyWord="HIRAGANA";
+//    private static String keyWord="HIRAGANA";
+
+    public static void main(String... args ) {
+//        String keyWord=args[0];
+//        int mx=args.length;
+//        if(mx%2!=0){
+//            nnn=1;
+//            System.exit(nnn);
+//        }else{
+//            s=Integer.parseInt(args[0]);
+//            e=Integer.parseInt(args[1]);
+//        }
+//        int s=0;
+//        int e=0xFF;
+        int s=0;
+        int e=0x10FFFF;
+//        int s=0x3040;
+//        int e=0x30FF;
+//        int s=0x1F400;
+//        int e=0x1F4FF;
+        Map<Integer, List<String>> rt= mkTbl(s,e);
+        Map<Integer, String> m = getCol(rt,3);
+
+        Integer mn = m.entrySet().stream().filter(v->v.getValue().contains(keyWord)).min(Comparator.comparing(ee->ee.getKey())).get().getKey();
+        Integer mx = m.entrySet().stream().filter(v->v.getValue().contains(keyWord)).max(Comparator.comparing(ee->ee.getKey())).get().getKey();
+
+        Map<Integer, List<String>> ret= mkTbl(mn,mx);
+
+        printOut(ret);
     }
 }
--- java/wrapper-exec.sh
+++ java/wrapper-exec.sh
@@ -3,18 +3,18 @@
 usage(){
 cat <<EOS
 Usage:
-$0 28
+$0 28 10
 
 EOS
 
 exit
 }
 
-[ -z $@ ] && usage
+N=$(printf "%05d" $1);shift
+CMD_ARGS="$@"
 
-N=$(printf "%05d" $@)
-[ -z $N ] && usage
+[ -z "$N" ] && usage
 
-time java -cp "$(find $N* -type d -name "nnn")/out:$(find $HOME/.m2 -type f -name "*jar"|xargs|tr ' ' ':')" app/App
+time java -cp "$(find $N* -type d -name "nnn")/out:$(find $HOME/.m2 -type f -name "*jar"|xargs|tr ' ' ':')" app/App $CMD_ARGS
 
 #time java -cp "$(find $N* -type d -name "nnn")/out:$(find $HOME/.m2 -type f -name "*jar"|xargs|tr ' ' ':')" app/AppTest
