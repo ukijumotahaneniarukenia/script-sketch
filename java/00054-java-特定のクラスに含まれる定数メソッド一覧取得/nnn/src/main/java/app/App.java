b3ee468c40d757f8e77884079e303bc9ca37d635
ukijumotahaneniarukenia <mrchildrenkh1008@gmail.com>
2020-04-22 13:46:20 +0900

    sketch

---
+++ java/00054-java-特定のクラスに含まれる定数メソッド一覧取得/nnn/src/main/java/app/App.java
@@ -0,0 +1,132 @@
+package app;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
+import java.util.*;
+import java.util.concurrent.Callable;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class App {
+    private final static String F = "---";
+    private final static String R = "###";
+    private final static String C = ",";
+    private final static String CONST_SIGN = "CCCCC";
+    private final static String METHOD_SIGN = "MMMMM";
+    private final static String OFS = "\t";
+    private final static String RS = "\n";
+    private final static String CLASS_GRP_DIGIT = "5";
+    private final static String CLASS_GRPSEQ_DIGIT = "5";
+
+    public static Map<Integer,String> ccc = hhh(IntStream.rangeClosed(0,1).boxed().collect(Collectors.toList()), Arrays.asList("クラス名" ,"定数名"));
+    public static Map<Integer,String> mmm = hhh(IntStream.rangeClosed(0,9).boxed().collect(Collectors.toList()), Arrays.asList(
+            "クラス名"
+            ,"アクセス修飾子"
+            ,"戻り値の型"
+            ,"メソッド名"
+            ,"可変長引数があるか"
+            ,"引数の個数"
+            ,"型パラメータリスト"
+            ,"型パラメータ記号リスト"
+            ,"引数の型リスト"
+            ,"仮引数の変数名リスト")
+    );
+    private static Map<Integer,String> hhh(List<Integer> k,List<String> v){
+        if(k.size()!=v.size()){
+            return new HashMap<>();
+        }else{
+            return k.stream().collect(Collectors.toMap(e->e, e->v.get(e)));
+        }
+    }
+    public static <T> T uncheckCall(Callable<T> callable) {
+        //パールの暗黙の変数みたいなものだろう。すべてのコンテキストに含まれている隠しオブジェクト。callableはすべての例外をグルーピングしている
+        //これがいちばんすごいわ
+        //https://blog1.mammb.com/entry/2015/03/31/001620
+        try {
+            return callable.call();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+    private static final ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+
+    public static void main( String[] args ) throws ClassNotFoundException {
+        Map<Class<?>,String> m = Arrays.asList("java.lang.Thread","java.lang.ClassLoader").stream()
+                .map(e->uncheckCall(()->classLoader.loadClass(e))).collect(Collectors.toMap(ee->ee,ee->ee.getName()));
+        fileWriteOut(uuu(m));
+    }
+    private static void fileWriteOut (Map<String,List<String>> m){
+        final String SUFFIX = ".tsv";
+        String preFileName = m.entrySet().stream().limit(1).map(e->e.getKey().split(F)[0]).collect(Collectors.joining()) + SUFFIX;
+        try{
+            File destFile = new File(preFileName);
+            if(destFile.exists()){
+                destFile.delete();
+            }
+            FileWriter fw = new FileWriter(preFileName, true);
+            //前回成果物ファイルを削除
+            for(Map.Entry<String,List<String>> entry : m.entrySet()) {
+                if (!preFileName.equals(entry.getKey().split(F)[0])) {
+                    fw.close();
+                    destFile = new File(entry.getKey().split(F)[0] + SUFFIX);
+                    destFile.delete();
+                    fw = new FileWriter(entry.getKey().split(F)[0] + SUFFIX, true);
+                }
+                fw.write(Arrays.asList(entry.getKey().split(F)).stream().collect(Collectors.joining(OFS))+OFS+entry.getValue().stream().collect(Collectors.joining(OFS)));
+                fw.write(RS);
+                preFileName = entry.getKey().split(F)[0];
+            }
+            fw.close();
+        }catch(IOException e){
+            e.printStackTrace();
+        }
+    }
+    private static Map<Method,Class<?>> ggg(Class<?> e){
+        List<Method> l = Arrays.asList(e.getMethods());
+        return IntStream.rangeClosed(0,l.size()-1).boxed().parallel().collect(Collectors.toMap(i->l.get(i),i->e));
+    }
+    private static Map<Field,Class<?>> ddd(Class<?> e){
+        List<Field> l = Arrays.asList(e.getFields());
+        return IntStream.rangeClosed(0,l.size()-1).boxed().parallel().collect(Collectors.toMap(i->l.get(i),i->e));
+    }
+    private static Map<String,List<String>> uuu(Map<Class<?>,String> m){
+        Map<String,List<String>> rt = new LinkedHashMap<>();
+        int grp=0;
+        int cnt=0;
+        for(Map.Entry<Class<?>,String> eeeee : m.entrySet()){
+            Class<?> clz = eeeee.getKey();
+            Map<Field,Class<?>> rtF = ddd(clz);
+            ++grp;
+            for(Map.Entry<Field,Class<?>> entry : rtF.entrySet()){
+                ++cnt;
+                rt.put(eeeee.getValue()+F+CONST_SIGN+F+String.format("%0"+CLASS_GRP_DIGIT+"d",grp)+F+String.format("%0"+CLASS_GRPSEQ_DIGIT+"d",cnt)
+                        , Arrays.asList(
+                                String.valueOf(entry.getValue().getName())//クラス名
+                                ,String.valueOf(entry.getKey().getName()))//定数名
+                );
+            }
+            Map<Method,Class<?>> rtM = ggg(clz);
+            for(Map.Entry<Method,Class<?>> entry : rtM.entrySet()){
+                ++cnt;
+                rt.put(eeeee.getValue()+F+METHOD_SIGN+F+String.format("%0"+CLASS_GRP_DIGIT+"d",grp)+F+String.format("%0"+CLASS_GRPSEQ_DIGIT+"d",cnt)
+                        ,Arrays.asList(
+                                entry.getValue().getName()//クラス名
+                                , Modifier.toString(entry.getKey().getModifiers())//アクセス修飾子
+                                ,entry.getKey().getGenericReturnType().getTypeName()//戻り値の型
+                                ,entry.getKey().getName()//メソッド名
+                                ,String.valueOf(entry.getKey().isVarArgs())//可変長引数があるか
+                                ,String.valueOf(entry.getKey().getParameterCount())//引数の個数
+                                ,Arrays.stream(entry.getKey().getTypeParameters()).flatMap(e->Arrays.asList(e.getBounds()).stream()).map(ee->ee.getTypeName().replace(C,R)).collect(Collectors.joining(C))//型パラメータリスト
+                                ,Arrays.stream(entry.getKey().getTypeParameters()).map(e->e.getTypeName().replace(C,R)).collect(Collectors.joining(C))//型パラメータで使用しているアルファベット大文字記号リスト
+                                ,Arrays.stream(entry.getKey().getGenericParameterTypes()).map(e->e.getTypeName().replace(C,R)).collect(Collectors.joining(C)) //引数の型リスト
+                                ,Arrays.stream(entry.getKey().getParameters()).map(e->e.getName()).collect(Collectors.joining(C))//仮引数の変数名リスト
+                        ));
+            }
+        }
+        return rt;
+    }
+}
