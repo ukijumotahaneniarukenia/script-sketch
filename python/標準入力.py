6d6a9ed0bef34a5f5edd082cc11052e88015b680
ukijumotahaneniarukenia <mrchildrenkh1008@gmail.com>
2020-05-06 20:48:06 +0900

    sketch

--- python/標準入力.py
+++ python/標準入力.py
@@ -1,80 +1,29 @@
-#!/usr/local/bin/python3.7
-
-#$seq 3 | ./標準入力.py
-#https://codeday.me/jp/qa/20190124/156164.html
-#https://note.nkmk.me/python-file-io-open-with/
-
-#エラーハンドリング
-#https://docs.python.org/ja/3/library/signal.html
-#https://www.m3tech.blog/entry/python-snippets
+#!/usr/bin/env python3
 import sys
-
 
 def usage():
     usage="""
 Usage:
-$seq 2 | ./標準入力.py
-start
-['1', '2']
-end
-$seq 1 | ./標準入力.py
-start
-['1']
-end
-$./標準入力.py {a..c}
-start
-['a', 'b', 'c']
-end
-$./標準入力.py $(seq 2)
-start
-['1', '2']
-end
-$./標準入力.py "$(seq 2)"
-start
-['1\\n2']
-end
-$cat a | ./標準入力.py 
-start
-['1 2 3 4 5', '6 7 8 9 10']
-end
-$./標準入力.py $(cat a)
-start
-['1', '2', '3', '4', '5', '6', '7', '8', '9', '10']
-end
-$./標準入力.py "$(cat a)"
-start
-['1 2 3 4 5\\n6 7 8 9 10']
-end
 
 """
     print(usage)
-    post_process("end")
-    sys.exit(1)
-
-def pre_process(rsv_args):
-    print(rsv_args)
-
-def post_process(rsv_args):
-    print(rsv_args)
     sys.exit(0)
 
-def chk_args():
+
+def mock(args):
+  for e in args:
+      print(e)
+
+def main():
   try:
-    if (len(sys.argv[1:]))==0:
-      #パイプ経由引数の場合
-      #改行を取り除く
-      print(list(map(lambda x:x.strip(),sys.stdin.readlines())))
-    else:
-      #コマンドライン経由引数の場合
-      #先頭要素のファイル名を除く
-      print(sys.argv[1:])
+    if (len(sys.argv[1:]))==0:#パイプ経由引数の場合
+      #print(list(map(lambda x:x.strip(),sys.stdin.readlines())))
+      args=list(map(lambda x:x.strip(),sys.stdin.readlines()))[0].split()
+      mock(args)
+    else:#コマンドライン経由引数の場合
+      usage()
   except KeyboardInterrupt:
     usage()
 
-def main():
-    chk_args()
-
 if __name__=="__main__":
-    pre_process("start")
     main()
-    post_process("end")
