0a06895b3230f47009b49908a40a00c5459ce422
ukijumotahaneniarukenia <mrchildrenkh1008@gmail.com>
2020-05-09 22:20:05 +0900

    sketch

--- java/00062-名前付き正規表現でHTMLタグ閉じタグ補完/nnn/src/main/java/App.java
+++ java/00062-名前付き正規表現でHTMLタグ閉じタグ補完/nnn/src/main/java/App.java
@@ -1,30 +1,37 @@
+import java.util.Arrays;
+import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-//$echo '<meta charset="utf-8" />' | perl -nlE 's;(\<meta)(.+)(/>);<meta \2></meta>;g and say'
-//<meta  charset="utf-8" ></meta>
-
-//Output
-//(?<startTag><meta)(?<text>.+)(?<endTag>/>)
-//true
-//<meta charset="utf-8" />
-//<meta  charset="utf-8" ></meta>
-
 public class App {
     public static void main(String[] args) {
-        Pattern p = Pattern.compile("(?<startTag><meta)(?<text>.+)(?<endTag>/>)");
-        String s = "<meta charset=\"utf-8\" />";
-        Matcher mc = p.matcher(s);
 
-        System.out.println(p);
+        List<String> noneCloseTagList = Arrays.asList("meta","link");
 
-        System.out.println(mc.find());
-
-        System.out.println(mc.group());
-
-        String done = mc.replaceAll("${startTag} ${text}></meta>");
-
-        System.out.println(done);
-
+//        String html = "<meta charset=\"utf-8\" />";
+//        String html = "<meta  charset=\"utf-8\" /><title>ukijumotahaneniarukenia - Qiita</title><meta content=\"width=device-width,initial-scale=1,shrink-to-fit=no\" name=\"viewport\" />";
+//        String html = "<link  href=\"/manifest.json\" rel=\"manifest\" />";
+        String html = "<html><head ><meta  charset=\"utf-8\" /><title>ukijumotahaneniarukenia - Qiita</title><meta content=\"width=device-width,initial-scale=1,shrink-to-fit=no\" name=\"viewport\" />" +
+                "<meta content=\"#55c500\" name=\"theme-color\" />" +
+                "<meta content=\"XWpkTG32-_C4joZoJ_UsmDUi-zaH-hcrjF6ZC_FoFbk\" name=\"google-site-verification\" />" +
+                "<link  href=\"/manifest.json\" rel=\"manifest\" />" +
+                "<link href=\"/opensearch.xml\" rel=\"search\" title=\"Qiita\" type=\"application/opensearchdescription+xml\" />]" +
+                "<meta name=\"csrf-param\" content=\"authenticity_token\" /></meta></link></head>" +
+                "<meta  name=\"csrf-token\" content=\"0Lc1YtC0JUoWUpbQ+yb/MU4WGhq3J3dbjOdrJ2cfq9jB/aSZhYbrbSyz01HJukTxviTVTDdw1er36M+rhKyw8Q==\" />" +
+                "<link  rel=\"shortcut icon\" type=\"image/x-icon\" href=\"https://cdn.qiita.com/assets/favicons/public/production-c620d3e403342b1022967ba5e3db1aaa.ico\" />" +
+                "<link rel=\"apple-touch-icon\" type=\"image/png\" href=\"https://cdn.qiita.com/assets/favicons/public/apple-touch-icon-ec5ba42a24ae923f16825592efdc356f.png\" />" +
+                "<link rel=\"stylesheet\" media=\"all\" href=\"https://cdn.qiita.com/assets/public/style-1766f40d5cba9699a6953f3bf6d6336d.min.css\" />";
+        System.out.println(html);
+        for(String tag:noneCloseTagList){
+            String detectNoneCloseTagRegexpPattern = "(?<startTag>(?<=<)"+tag+")(?<text>.*?)(?<endTag>/?>)";
+            String completeNoneCloseTagRegexpPattern = "${startTag} ${text}></${startTag}>";
+            Pattern p = Pattern.compile(detectNoneCloseTagRegexpPattern);
+            Matcher mc = p.matcher(html);
+            System.out.println("＠＠＠＠＠＠＠＠＠＠＠＠＠＠＠＠＠＠＠＠＠＠＠＠＠＠＠＠＠＠＠＠＠＠＠＠＠＠＠＠＠＠＠＠＠＠＠＠＠＠＠＠＠＠＠＠＠＠＠＠＠＠＠＠＠＠＠＠＠＠＠＠＠＠＠＠＠＠＠＠＠＠＠＠＠＠＠＠＠＠＠＠＠＠＠＠＠＠＠＠");
+            html = mc.replaceAll(completeNoneCloseTagRegexpPattern);
+            System.out.println(html);
+//            System.out.println(detectNoneCloseTagRegexpPattern);
+//            System.out.println(completeNoneCloseTagRegexpPattern);
+        }
     }
 }
