8105ab9e7502ebde8ef151443b6e987c9703e8ef
ukijumotahaneniarukenia <mrchildrenkh1008@gmail.com>
2020-05-09 19:09:19 +0900

    sketch

--- java/00061-java-単一実行クローラ/nnn/src/main/java/app/App.java
+++ java/00061-java-単一実行クローラ/nnn/src/main/java/app/App.java
@@ -6,6 +6,9 @@ import edu.uci.ics.crawler4j.fetcher.PageFetcher;
 import edu.uci.ics.crawler4j.robotstxt.RobotstxtConfig;
 import edu.uci.ics.crawler4j.robotstxt.RobotstxtServer;
 
+//Usage:
+// kuraine docker-container-centos-7-6-18-10-java-gradle-idea ~$java -cp "$HOME/script-sketch/java/00061-java-単一実行クローラ/nnn/build/libs/nnn-1.0-SNAPSHOT.jar:$(find $HOME/.gradle/caches/modules-2/files-2.1 -name "*jar"|xargs|tr ' ' ':')" app/App
+
 public class App{
 
     private static final Integer REQUEST_TIME_INTERVAL=10000;  // 10秒ごとにリクエスト
@@ -14,19 +17,43 @@ public class App{
 
     private static final String DEFAULT_CACHE_DIR = "/home/kuraine/cache";
 
+    private static final String DEFAULT_CSS_SELECTOR_QUERY = ".section .block dl dt,a"; // sectionクラスの下のblockクラスの下のdlノードの下のdtまたはaノードを抽出
+
     private static final String DEFAULT_IGNORE_SUFFIX_REGEXP_PATTERN = ".*(\\.(css|js|gif|jpg|png|mp3|mp3|zip|gz))$";
 
     //単一設定のみハンドリングできる模様 呼出側のシェルで複数件対応する
+//    public static final AppConfig APP_CONFIG = new AppConfig(
+//            "https://dev.to/t/python"
+////            ,DEFAULT_CSS_SELECTOR_QUERY
+//            ,".small-pic-link-wrapper"
+//            ,DEFAULT_IGNORE_SUFFIX_REGEXP_PATTERN
+//            ,DEFAULT_CACHE_DIR
+//    );
+
+    //単一設定のみハンドリングできる模様 呼出側のシェルで複数件対応する
     public static final AppConfig APP_CONFIG = new AppConfig(
-            "https://ukijumotahaneniarukenia.site/"
+            "https://qiita.com/ukijumotahaneniarukenia"
+//            ,DEFAULT_CSS_SELECTOR_QUERY
+            ,"a"
             ,DEFAULT_IGNORE_SUFFIX_REGEXP_PATTERN
             ,DEFAULT_CACHE_DIR
     );
+//    //単一設定のみハンドリングできる模様 呼出側のシェルで複数件対応する
+//    public static final AppConfig APP_CONFIG = new AppConfig(
+//            "https://ukijumotahaneniarukenia.site/"
+////            ,DEFAULT_CSS_SELECTOR_QUERY
+//            ,"a"
+//            ,DEFAULT_IGNORE_SUFFIX_REGEXP_PATTERN
+//            ,DEFAULT_CACHE_DIR
+//    );
 
     public static void main(String[] args) throws Exception {
         CrawlController controller = setCrawlConfig(APP_CONFIG);
         System.out.println("Start Crawler");
         controller.start(MyCrawler.class, THREAD_CNT);
+
+
+
     }
 
     private static CrawlController setCrawlConfig(AppConfig appConfig) throws Exception {
--- java/00061-java-単一実行クローラ/nnn/src/main/java/app/AppConfig.java
+++ java/00061-java-単一実行クローラ/nnn/src/main/java/app/AppConfig.java
@@ -6,20 +6,26 @@ import java.util.Map;
 public class AppConfig {
 
     private static final String BASE_URL_MAP = "BASE_URL_MAP";
+    private static final String CSS_SELECTOR_MAP = "CSS_SELECTOR_MAP";
     private static final String IGNORE_SUFFIX_MAP = "IGNORE_SUFFIX_MAP";
     private static final String CACHE_DIR_MAP = "CACHE_DIR_MAP";
 
     private Map<String,String> baseUrlMap = new HashMap<>();
+    private Map<String,String> cssSelectorMap = new HashMap<>();
     private Map<String,String> ignoreSuffixMap = new HashMap<>();
     private Map<String,String> cacheDirMap = new HashMap<>();
 
-    public AppConfig(String baseUrl,String ignoreSuffix,String cacheDir){
+    public AppConfig(String baseUrl,String cssSelector,String ignoreSuffix,String cacheDir){
         baseUrlMap.put(BASE_URL_MAP,baseUrl);
+        cssSelectorMap.put(CSS_SELECTOR_MAP,cssSelector);
         ignoreSuffixMap.put(IGNORE_SUFFIX_MAP,ignoreSuffix);
         cacheDirMap.put(CACHE_DIR_MAP,cacheDir);
     }
     public String getBaseUrl() {
         return baseUrlMap.get(BASE_URL_MAP);
+    }
+    public String getCssSelector() {
+        return cssSelectorMap.get(CSS_SELECTOR_MAP);
     }
     public String getIgnoreSuffixMap() {
         return ignoreSuffixMap.get(IGNORE_SUFFIX_MAP);
