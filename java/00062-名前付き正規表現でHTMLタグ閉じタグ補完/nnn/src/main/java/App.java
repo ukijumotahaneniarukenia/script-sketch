ac9f52f63e02674f5a9d4723fb34ba598211b1a5
ukijumotahaneniarukenia <mrchildrenkh1008@gmail.com>
2020-05-10 16:10:59 +0900

    sketch

--- java/00062-名前付き正規表現でHTMLタグ閉じタグ補完/nnn/src/main/java/App.java
+++ java/00062-名前付き正規表現でHTMLタグ閉じタグ補完/nnn/src/main/java/App.java
@@ -12,27 +12,52 @@ public class App {
 
         List<String> incorrectPairTagList = Arrays.asList("option","div");
 
-//        String html = "<div class=\"position-relative\">    </option><option class=\"position-relative\">    </option><option class=\"position-relative\">    </div>"; //開始タグの開始忘れ
+        String html = "<div class=\"position-relative\">    </option><option class=\"position-relative\">    </option><option class=\"position-relative\">    </div>"; //開始タグの開始忘れ
 
-//        String html = "<div class=\"position-relative\">    </option>"; //開始タグの開始忘れ
-
-//        String html = "<div  class=\"position-relative\"></div>    </option>"; //開始タグの開始忘れ
-
-        String html = "<div id=\"body-styles\"></div>";// TODO 閉じタグ補完の処理でこのパターンをうまくハンドリングしたい これがうまくできれば突破できそう
-
+//        String html = "<div class=\"position-relative\">    </option>"; //タグの不一致 -->"<div class=\"position-relative\">    </div>"
+//        String html = "<div  class=\"position-relative\"></div>    </option>"; //開始タグの開始忘れ -->"<div  class=\"position-relative\"></div><option>    </option>"
+//        String html = "<div id=\"body-styles\"></div>";// 変化しないことを確認
 //        String html = "<meta charset=\"utf-8\" />"; //閉じタグの忘れ
 //        String html = "<meta  charset=\"utf-8\" /><title>ukijumotahaneniarukenia - Qiita</title><meta content=\"width=device-width,initial-scale=1,shrink-to-fit=no\" name=\"viewport\" />"; //閉じタグの忘れ
 //        String html = "<link  href=\"/manifest.json\" rel=\"manifest\" />"; //閉じタグの忘れ
 
-
         System.out.println(html);
-        //開始タグの補完
+//
+//        //開始タグの補完
         for(String tag:noneStartTagList){
             String detectNoneStartTagRegexpPattern = "(?<preCloseTag></.*?>)(?<text>.*?)(?<closeTag>(?<=</)"+tag+"(?=>))";
             String completeNoneStartTagRegexpPattern = "${preCloseTag}<${closeTag}>${text}${closeTag}";
             Pattern p = Pattern.compile(detectNoneStartTagRegexpPattern);
             Matcher mc = p.matcher(html);
-            html = mc.replaceAll(completeNoneStartTagRegexpPattern);
+            while (mc.find()){
+                if(mc.group("text").matches("[^<>]*</$")){
+                    //以下を区別している
+
+//                    text	    </
+//                    text	<option class="position-relative">    </
+
+                    html = mc.replaceAll(completeNoneStartTagRegexpPattern);
+                }else{
+
+                }
+            }
+        }
+
+        System.out.println(html);
+
+        //閉じタグの補完
+        for(String tag:noneCloseTagList){
+            String detectNoneCloseTagRegexpPattern = "(?<startTag>(?<=<)"+tag+")(?<text>.*?)(?<endTag>/?>)";
+            String completeNoneCloseTagRegexpPattern = "${startTag} ${text}></${startTag}>";
+            Pattern p = Pattern.compile(detectNoneCloseTagRegexpPattern);
+            Matcher mc = p.matcher(html);
+            while(mc.find()){
+                if(mc.group("endTag").equals("/>")){
+                    html = mc.replaceAll(completeNoneCloseTagRegexpPattern);
+                }else{
+                    //閉じ忘れていないので、なにもしない
+                }
+            }
         }
 
         System.out.println(html);
@@ -50,18 +75,7 @@ public class App {
                 }
             }
         }
-
         System.out.println(html);
 
-        //閉じタグの補完
-        for(String tag:noneCloseTagList){
-            String detectNoneCloseTagRegexpPattern = "(?<startTag>(?<=<)"+tag+")(?<text>.*?)(?<endTag>/?>)";
-            String completeNoneCloseTagRegexpPattern = "${startTag} ${text}></${startTag}>";
-            Pattern p = Pattern.compile(detectNoneCloseTagRegexpPattern);
-            Matcher mc = p.matcher(html);
-            html = mc.replaceAll(completeNoneCloseTagRegexpPattern);
-        }
-
-        System.out.println(html);
     }
 }
