5bf990f54b1cbc52992e60a874a69a2a21638802
ukijumotahaneniarukenia <mrchildrenkh1008@gmail.com>
2020-05-11 22:44:35 +0900

    sketch

---
+++ c#/00009-c#-monodevelopでの外部ライブラリ使用プロジェクト作成/app/nnn/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Xml.Linq;
+using Sgml;
+
+
+//http://neue.cc/2010/03/02_244.html
+
+namespace nnn
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+
+            Console.WriteLine(ParseHtml("https://ukijumotahaneniarukenia.site/"));
+
+        }
+        private static XDocument ParseHtml(String url)
+        {
+            var sgmlReader = new SgmlReader
+            {
+                Href = url
+            };
+
+            {
+                return XDocument.Load(sgmlReader);
+            }
+        }
+    }
+}
---
+++ c#/00009-c#-monodevelopでの外部ライブラリ使用プロジェクト作成/md-dev.md
@@ -0,0 +1,8 @@
+urlからsgmlReader経由でhtmlをxmlに変換する例
+
+
+xmlになっているかはlintでチェック
+
+```
+$mono ./app/nnn/bin/Debug/nnn.exe  | xmllint --format -
+```
