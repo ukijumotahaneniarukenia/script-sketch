efc5d548169940883c7fc9b9957ee52be71c3560
ukijumotahaneniarukenia <mrchildrenkh1008@gmail.com>
2020-05-18 09:58:23 +0900

    sketch

--- pocket/00002-pocket-タイトルとURL情報を取得.sh
+++ pocket/00002-pocket-タイトルとURL情報を取得.sh
@@ -14,3 +14,6 @@ OUTPUT_FILE_NAME=pocket.tsv
 rm -f $OUTPUT_FILE_NAME
 
 cat $INPUT_FILE_NAME | jq --stream -c '' | grep -P 'resolved_url|resolved_title' | jq -c '{key:.[0]|join("-"),value:.[1]}|select(.value!="")|{grp:.key|gsub("\\D+";""),key:.key|gsub(".*-";"")|gsub(".*_";""),value:.value}' | jq -s 'group_by(.grp)[]|.[].grp as $grp|reduce .[].value as $item([];.+[$item])|{title:.[0],url:.[1]}' | jq -s 'unique' | jq -r '.[]|[.title,.url]|@tsv' | sed '1iタイトル\tURL' > $OUTPUT_FILE_NAME
+
+
+sed -i -r 's/\x22//g;' $OUTPUT_FILE_NAME
