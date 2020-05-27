da0f417f6689fa6a5ea33c002478625599b5ae3a
ukijumotahaneniarukenia <mrchildrenkh1008@gmail.com>
2020-05-28 00:28:44 +0900

    sketch

---
+++ go/00002-go-自作パッケージ外部レポインクルード/src/main/main.go
@@ -0,0 +1,11 @@
+package main
+import (
+    "fmt"
+    nnn "github.com/ukijumotahaneniarukenia/go-repo/gosample"
+    mmm "github.com/ukijumotahaneniarukenia/go-repo/callee"
+)
+
+func main() {
+    fmt.Println(nnn.Message)
+    fmt.Println(mmm.MSG)
+}
