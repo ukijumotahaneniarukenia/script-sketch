ef659f34be7f347202667856b8486a8991cce62d
ukijumotahaneniarukenia <mrchildrenkh1008@gmail.com>
2020-05-31 00:18:38 +0900

    コンポーネント化するまえの状態をコミット

--- kotlin/00004-kotlin-kotlinバンドルのjsでreactフレームワークを使用したアプリ/build.gradle.kts
+++ kotlin/00004-kotlin-kotlinバンドルのjsでreactフレームワークを使用したアプリ/build.gradle.kts
@@ -22,6 +22,7 @@ dependencies {
     implementation(npm("react-dom", "16.13.1"))
 
     //Kotlin Styled (chapter 3)
+    //cssのスタイリングで必要
     implementation("org.jetbrains:kotlin-styled:1.0.0-pre.94-kotlin-1.3.70")
     implementation(npm("styled-components"))
     implementation(npm("inline-style-prefixer"))
--- kotlin/00004-kotlin-kotlinバンドルのjsでreactフレームワークを使用したアプリ/src/main/kotlin/Main.kt
+++ kotlin/00004-kotlin-kotlinバンドルのjsでreactフレームワークを使用したアプリ/src/main/kotlin/Main.kt
@@ -1,9 +1,46 @@
 import react.dom.*
 import kotlin.browser.document
 
+import kotlinx.css.*
+import styled.*
+
+//データ構造Bean定義
+data class Video(val id: Int, val title: String, val speaker: String, val videoUrl: String)
+
+//メモリ上にリスト定義
+val unwatchedVideos = listOf(
+    Video(1, "Building and breaking things", "ロバートデニーロ", "https://youtu.be/PsaFVLr8t4E"),
+    Video(2, "The development process", "レオナルド・ディカプリオ", "https://youtu.be/PsaFVLr8t4E"),
+    Video(3, "The Web 7.0", "森鴎外", "https://youtu.be/PsaFVLr8t4E")
+)
+
+//メモリ上にリスト定義
+val watchedVideos = listOf(
+    Video(4, "Mouseless development", "ベン・ジョンソン", "https://youtu.be/PsaFVLr8t4E")
+)
+
+
 fun main() {
 //    document.bgColor = "blue"
     render(document.getElementById("root")){
+
+        styledDiv {
+            //指定した書式で配置するタグをstyledDivブロックで囲んでブラウザにレンダラさせる
+            css {
+                position = Position.absolute
+                top = 10.px
+                right = 10.px
+            }
+            h3 {
+                +"John Doe: Building and breaking things"
+            }
+            img {
+                attrs {
+                    src = "https://via.placeholder.com/640x360.png?text=Video+Player+Placeholder"
+                }
+            }
+        }
+
         h1 {
             +"KotlinConf Explorer"
         }
@@ -28,15 +65,34 @@ fun main() {
                 +"Tom Jerry: Mouseless development"
             }
         }
-        div {
-            h3 {
-                +"John Doe: Building and breaking things"
-            }
-            img {
-                attrs {
-                    src = "https://via.placeholder.com/640x360.png?text=Video+Player+Placeholder"
-                }
+
+        div{
+            + "まだ見ていいない動画リスト"
+        }
+        //動的にタグ定義
+        for(video in unwatchedVideos) {
+            p {
+                +"${video.speaker}: ${video.title}"
             }
         }
+        div{
+            + "見た動画リスト"
+        }
+        //動的にタグ定義
+        for(video in watchedVideos) {
+            p {
+                +"${video.speaker}: ${video.title}"
+            }
+        }
+//        div {
+//            h3 {
+//                +"John Doe: Building and breaking things"
+//            }
+//            img {
+//                attrs {
+//                    src = "https://via.placeholder.com/640x360.png?text=Video+Player+Placeholder"
+//                }
+//            }
+//        }
     }
 }
