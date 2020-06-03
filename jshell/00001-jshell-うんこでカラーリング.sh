4c52e274c4adfff66481ae819410e5bfb40211b7
ukijumotahaneniarukenia <mrchildrenkh1008@gmail.com>
2020-06-04 01:31:09 +0900

    sketch

--- jshell/00001-jshell-うんこでカラーリング.sh
+++ jshell/00001-jshell-うんこでカラーリング.sh
@@ -7,3 +7,18 @@ String s = "@";String t = "\033[38;5;"+"tobe"+"m"+s+"\033[0m";IntStream.rangeClo
 E
 )
 ' | tail -n+2 | sed '$d'|xargs -n16
+
+
+
+
+
+
+
+
+
+echo $s | xargs -I@ bash -c 'jshell -s < <(cat <<E
+String s = "@";String t = "\033[48;5;"+"tobe"+"m"+s+"\033[0m";IntStream.rangeClosed(1,255).boxed().forEach(i->System.out.println(t.replace("tobe",String.valueOf(i))));
+/exit
+E
+)
+' | tail -n+2 | sed '$d'|xargs -n16
