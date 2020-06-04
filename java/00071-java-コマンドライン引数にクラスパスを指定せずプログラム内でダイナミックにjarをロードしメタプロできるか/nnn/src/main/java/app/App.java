f07237c2cc989c27761e77382810e1935c44cc7f
ukijumotahaneniarukenia <mrchildrenkh1008@gmail.com>
2020-06-04 14:50:23 +0900

    sketch

--- java/00071-java-コマンドライン引数にクラスパスを指定せずプログラム内でダイナミックにjarをロードしメタプロできるか/nnn/src/main/java/app/App.java
+++ java/00071-java-コマンドライン引数にクラスパスを指定せずプログラム内でダイナミックにjarをロードしメタプロできるか/nnn/src/main/java/app/App.java
@@ -1,7 +1,6 @@
 package app;
 
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 import java.net.URL;
@@ -26,11 +25,11 @@ public class App {
 
     private static final ClassLoader parent = ClassLoader.getSystemClassLoader();;
 
-    private static Map<Method,Class<?>> getMethodInfo(Class<?> e){
+    private static Map<Method,Class<?>> getMethodInfo(Class<?> e) throws NoClassDefFoundError{
         List<Method> l = Arrays.asList(e.getMethods());
         return IntStream.rangeClosed(0,l.size()-1).boxed().parallel().collect(Collectors.toMap(i->l.get(i),i->e));
     }
-    private static Map<Field,Class<?>> getFieldInfo(Class<?> e){
+    private static Map<Field,Class<?>> getFieldInfo(Class<?> e) throws NoClassDefFoundError{
         List<Field> l = Arrays.asList(e.getFields());
         return IntStream.rangeClosed(0,l.size()-1).boxed().parallel().collect(Collectors.toMap(i->l.get(i),i->e));
     }
@@ -43,8 +42,9 @@ public class App {
         return Files.walk(baseDir)
                 .parallel()
                 .map(e -> e.toFile())
-                .filter(e -> e.isFile())
-                .filter(e -> e.getAbsolutePath().matches(includeExtensionPtn))
+                .filter(e ->e.isFile())
+                .filter(e ->e.getAbsolutePath().matches(includeExtensionPtn))
+                .filter(e->!e.getAbsolutePath().contains("sources"))
                 .collect(Collectors.toSet());
     }
     private static URLClassLoader newClassLoader(Set<File> files) {
@@ -59,11 +59,12 @@ public class App {
         }
     }
 
-    //TODO 除外単語ファイルをリスト化してパッケージ名に含まれていれば除外する
+    private static final String jarFileClassLoadDoneFileName = "jarFileClassLoadDone.tsv";
+    private static final String jarFileClassLoadSkipFileName = "jarFileClassLoadSkip.tsv";
 
-    public static void main(String... args) throws IOException, ReflectiveOperationException {
+    public static void main(String... args) throws IOException {
 
-        String defaultBaseDir = "/home/kuraine/.gradle/caches/modules-2/files-2.1";
+        String defaultBaseDir = "/home/kuraine/.gradle/caches/modules-2/files-2.1/";
 
         if(args.length>2){
             System.exit(1);
@@ -80,50 +81,82 @@ public class App {
 
         int jarFileListCnt = jarFileList.size();
         int jarFileClassCnt = 0;
-        int jarFileClassLoadCnt = 0;
+        int jarFileClassLoadDoneCnt = 0;
         int jarFileClassLoadSkipCnt = 0;
+
+        Map<String,List<String>> classLoadDoneResult = new LinkedHashMap<>();
+        Map<String,List<String>> classLoadSkipResult = new LinkedHashMap<>();
 
         for (File f : jarFileList) {
             JarFile jarFile = new JarFile(f.getPath());
             List<JarEntry> jarEntries = Collections.list(jarFile.entries());
+            List<String> classLoadList = new ArrayList<>();
+            List<String> classLoadSkipList = new ArrayList<>();
             for (JarEntry jarEntry : jarEntries) {
                 if (jarEntry.getName().endsWith(".class")) {
                     jarFileClassCnt++;
                     String className = jarEntry.getName().replace('/', '.').replaceAll(".class$", "");
-                    if(
-                            //パッケージ名などから除外対象をある程度予測する
-                            //その時点での依存関係をすべて解決できる必要はないので
-
-                            //xmlやjmsとか絡んできたら、泥試合確定なので、割りっきって
-
-                            jarEntry.getName().contains("module-info")
-                            ||jarEntry.getName().contains("META-INF") //Exception in thread "main" java.lang.NoClassDefFoundError: org/apache/logging/log4j/core/util/SystemClock (wrong name: META-INF/versions/9/org/apache/logging/log4j/core/util/SystemClock)
-                            ||jarEntry.getName().contains("plugin") //Exception in thread "main" java.lang.NoClassDefFoundError: org/apache/logging/log4j/core/util/SystemClock (wrong name: META-INF/versions/9/org/apache/logging/log4j/core/util/SystemClock)
-
-                    ){
-                        //個別対応でいいか、複数のjarの機能を一気にすべて知りたいことあんまないし
-                        jarFileClassLoadSkipCnt++;
-                        continue;
-                    }else{
+                    try {
                         Class<?> loadClass = classLoader.loadClass(className);
 
-                        System.out.println(getFieldInfo(loadClass));
-                        System.out.println(getMethodInfo(loadClass));
+                        getFieldInfo(loadClass);
+                        getMethodInfo(loadClass);
 
-                        jarFileClassLoadCnt++;
+                        classLoadList.add(className);
+
+                        jarFileClassLoadDoneCnt++;
+                    }catch (ClassNotFoundException | NoClassDefFoundError e){
+                        classLoadSkipList.add(className);
+                        jarFileClassLoadSkipCnt++;
                     }
                 }
             }
+            classLoadDoneResult.put(f.getPath(),classLoadList);
+            classLoadSkipResult.put(f.getPath(),classLoadSkipList);
         }
+
+        File jarFileClassLoadDoneFile = new File(jarFileClassLoadDoneFileName);
+        if(jarFileClassLoadDoneFile.exists()){
+            jarFileClassLoadDoneFile.delete();
+        }else{
+            jarFileClassLoadDoneFile.createNewFile();
+        }
+
+        File jarFileClassLoadSkipFile = new File(jarFileClassLoadSkipFileName);
+        if(jarFileClassLoadSkipFile.exists()){
+            jarFileClassLoadSkipFile.delete();
+        }else{
+            jarFileClassLoadSkipFile.createNewFile();
+        }
+
+        OutputStreamWriter oswJarFileClassLoadDoneFileName  = new OutputStreamWriter(new FileOutputStream(jarFileClassLoadDoneFileName), "UTF-8");
+        BufferedWriter bwJarFileClassLoadDoneFileName = new BufferedWriter(oswJarFileClassLoadDoneFileName);
+        for(Map.Entry<String,List<String>> entry : classLoadDoneResult.entrySet()){
+            for(String className:entry.getValue()){
+                bwJarFileClassLoadDoneFileName.write(entry.getKey().replace(defaultBaseDir,"").replace("/",".")+"\t"+className);
+                bwJarFileClassLoadDoneFileName.newLine();
+            }
+        }
+        bwJarFileClassLoadDoneFileName.close();
+
+        OutputStreamWriter oswJarFileClassLoadSkipFileName  = new OutputStreamWriter(new FileOutputStream(jarFileClassLoadSkipFileName), "UTF-8");
+        BufferedWriter bwJarFileClassLoadSkipFileName = new BufferedWriter(oswJarFileClassLoadSkipFileName);
+        for(Map.Entry<String,List<String>> entry : classLoadSkipResult.entrySet()){
+            for(String className:entry.getValue()){
+                bwJarFileClassLoadSkipFileName.write(entry.getKey().replace(defaultBaseDir,"").replace("/",".")+"\t"+className);
+                bwJarFileClassLoadSkipFileName.newLine();
+            }
+        }
+        bwJarFileClassLoadSkipFileName.close();
 
         System.out.printf(
                 "%s\t%s\n" +
-                "%s\t%s\n" +
-                "%s\t%s\n" +
-                "%s\t%s\n"
+                        "%s\t%s\n" +
+                        "%s\t%s\n" +
+                        "%s\t%s\n"
                 ,"jarFileListCnt",jarFileListCnt
                 ,"jarFileClassCnt",jarFileClassCnt
-                ,"jarFileClassLoadCnt",jarFileClassLoadCnt
+                ,"jarFileClassLoadDoneCnt",jarFileClassLoadDoneCnt
                 ,"jarFileClassLoadSkipCnt",jarFileClassLoadSkipCnt
         );
 
