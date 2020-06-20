337ac6e708da743dfa87731a695db492ea403ce7
ukijumotahaneniarukenia <mrchildrenkh1008@gmail.com>
2020-06-20 09:01:10 +0900

    sketch

--- java/00080-java-外部コマンドの実行/src/main/java/app/App.java
+++ java/00080-java-外部コマンドの実行/src/main/java/app/App.java
@@ -4,20 +4,32 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class App {
+
+    private static final String SEPARATOR =  " ";
+    private static final String ORS =  "\n";
+
     public static void main(String[] args) {
-        System.out.println(execCmd());
+        System.out.println(execCmd("ls","うんこ","アイスクリーム"));
+        System.out.println(execCmd("ls"));
     }
-    private static String execCmd(String... cmdLineArgs){
+    private static String execCmd(String cmd, String... cmdLineArgs){
         List<String> rt = new LinkedList<>();
         try {
-            String cmd = "ls";
-            String[] cmdTemplate = {"/bin/bash", "-c", cmd};
-            Process process = new ProcessBuilder(cmdTemplate).start();
+            List<String> cmdTemplate = new LinkedList(){{
+                add("/bin/bash");
+                add("-c");
+                add(cmd + SEPARATOR + Arrays.asList(cmdLineArgs).stream().collect(Collectors.joining(SEPARATOR)));
+            }};
+
+            System.out.println(cmdTemplate.stream().collect(Collectors.joining(SEPARATOR)));
+
+            Process process = new ProcessBuilder(cmdTemplate.toArray(new String[cmdTemplate.size()])).start();
             InputStream inputStream = process.getInputStream();
             InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
             BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
@@ -30,6 +42,6 @@ public class App {
         } catch (IOException e) {
             e.printStackTrace();
         }
-        return rt.stream().collect(Collectors.joining("\n"));
+        return rt.stream().collect(Collectors.joining(ORS));
     }
 }
