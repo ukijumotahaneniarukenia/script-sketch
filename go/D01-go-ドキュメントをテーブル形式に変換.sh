e688ebff8ea1f05b466199f5647b8dfcc68179a5
ukijumotahaneniarukenia <mrchildrenkh1008@gmail.com>
2020-06-22 05:36:44 +0900

    sketch

--- go/D01-go-ドキュメントをテーブル形式に変換.sh
+++ go/D01-go-ドキュメントをテーブル形式に変換.sh
@@ -4,6 +4,6 @@ echo "https://golang.org/pkg/strings/" | ruby -ruri -rnet/http -anle '$F.map{|e|
 
 cat golang-pkg-strings.html | pup '#manual-nav' | sed '1i<?xml version="1.0" encoding="UTF-8"?>' >golang-pkg-strings.xml
 
-echo golang-pkg-strings.xml | ruby a.rb
+echo golang-pkg-strings.xml | xml2json
 
 cat golang-pkg-strings.json | jq --stream -c '{key:.[0]|map(if type == "number" then tostring else . end)|join("-"),value:.[1]}|select(.value!=null)' | jq -sr 'map({key:.key,value:.value|gsub("^\n +";"")|gsub("\n +$";"")})|.[]|[.key,.value]|@tsv'
