149bba515c5dd06c0817dfe5d8004309b0be6113
ukijumotahaneniarukenia <mrchildrenkh1008@gmail.com>
2020-06-22 06:20:21 +0900

    sketch

--- go/D01-go-ドキュメントをテーブル形式に変換.sh
+++ go/D01-go-ドキュメントをテーブル形式に変換.sh
@@ -38,7 +38,7 @@ echo golang-pkg-$FILE_NAME.xml | xml2json
 
 cat golang-pkg-$FILE_NAME.json | jq --stream -c '{key:.[0]|map(if type == "number" then tostring else . end)|join("-"),value:.[1]}|select(.value!=null)' | \
   jq -sr "map({pkg:\"$PKG_NAME\",key:.key,value:.value|gsub(\"^\n +\";\"\")|gsub(\"\n +$\";\"\")})|.[]|[.pkg,.key,.value]|@tsv" | \
-  sed '1iパッケージ名\tキー\tバリュー' >golang-pkg-$FILE_NAME.tsv
+  sed '1iパッケージ名\tキー\tバリュー' | sed "2i$PKG_NAME\tdiv-url\thttps://golang.org/pkg/$PKG_NAME/" >golang-pkg-$FILE_NAME.tsv
 
 
 ls golang* | grep -v tsv | xargs rm
