42ec897bd640d986c523dca9827d44c2c6cd2f6f
ukijumotahaneniarukenia <mrchildrenkh1008@gmail.com>
2020-06-24 06:27:02 +0900

    sketch

---
+++ go/00043-go-filepathでパスからファイル名を得る/src/main/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+  "path/filepath"
+)
+
+func main() {
+
+  a := "/home/kuraine/script-sketch/go/00043-go-filepathでパスからファイル名を得る/src/main/main.go"
+
+	fmt.Println(a)
+
+
+  fmt.Println(filepath.Base(a)) // main.go
+
+}
