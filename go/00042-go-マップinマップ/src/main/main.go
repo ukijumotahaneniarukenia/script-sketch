81b15d1d2646cad32a139107b32c93b3dbc344bd
ukijumotahaneniarukenia <mrchildrenkh1008@gmail.com>
2020-06-28 22:50:23 +0900

    sketch

--- go/00042-go-マップinマップ/src/main/main.go
+++ go/00042-go-マップinマップ/src/main/main.go
@@ -5,21 +5,18 @@ import (
 )
 
 func main() {
-  m := map[string]map[string]int{
-	  "00001": {"key": 10, "value": 20},
-	  "00002": {"key": 30, "value": 40},
-	  "00003": {"key": 50, "value": 60},
-  }
+	m := map[string]map[string]int{
+		"00001": {"key": 10, "value": 20},
+		"00002": {"key": 30, "value": 40},
+		"00003": {"key": 50, "value": 60},
+	}
 
-  fmt.Println(m["00001"]["key"]) // 10
+	fmt.Println(m["00001"]["key"]) // 10
 
+	fmt.Println(m["00001"]) // map[key:10 value:20]
 
+	fmt.Println(m["00002"]) // map[key:30 value:40]
 
-  fmt.Println(m["00001"]) // map[key:10 value:20]
-
-  fmt.Println(m["00002"]) // map[key:30 value:40]
-
-
-  fmt.Println(m["00003"]) // map[key:50 value:60]
+	fmt.Println(m["00003"]) // map[key:50 value:60]
 
 }
