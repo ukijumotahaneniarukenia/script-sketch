b47fbb8a39bf4d5be4794967d275ff853798cf20
ukijumotahaneniarukenia <mrchildrenkh1008@gmail.com>
2020-07-08 19:41:22 +0900

    sketch

---
+++ python/00025-python-ドット絵など/a.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+LFT_VERTICAL=2807
+LFT_CORNER=280b
+UPPER_HORIZONTAL=2819
+RGT_CORNER=2838
+RGT_VERTICAL=2834
+LOWER_HORIZONTAL=2826
+
+MESSAGE=" loading.."
+SLEEP_TIME=0.2
+
+while :
+do
+    printf "\e[32m\r\u$LFT_VERTICAL\e[0m"
+    sleep $SLEEP_TIME
+    printf "\e[32m\r\u$LFT_CORNER\e[0m $MESSAGE"
+    sleep $SLEEP_TIME
+    printf "\e[32m\r\u$UPPER_HORIZONTAL\e[0m $MESSAGE"
+    sleep $SLEEP_TIME
+    printf "\e[32m\r\u$RGT_CORNER\e[0m $MESSAGE"
+    sleep $SLEEP_TIME
+    printf "\e[32m\r\u$RGT_VERTICAL\e[0m $MESSAGE"
+    sleep $SLEEP_TIME
+    printf "\e[32m\r\u$LOWER_HORIZONTAL\e[0m $MESSAGE"
+    sleep $SLEEP_TIME
+done
