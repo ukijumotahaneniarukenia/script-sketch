233232d7df8afee53aa436085c3d5d1a923d1942
ukijumotahaneniarukenia <mrchildrenkh1008@gmail.com>
2020-07-18 23:23:15 +0900

    ブラウザのフォームに入力された値をポップアップで表示

--- React/00001-React-お試しアプリ/my-app/src/App.tsx
+++ React/00001-React-お試しアプリ/my-app/src/App.tsx
@@ -8,7 +8,10 @@ import FilterButton from "./components/FilterButton";
 
 function App(props:any) {
 
-  
+  function addTask(name:any) {
+    alert(name);
+  }
+
   const taskList = props.tasks.map((task:any) => (
     <Todo
       id={task.id}
@@ -21,43 +24,11 @@ function App(props:any) {
   return (
     <div className="todoapp stack-large">
       <h1>TodoMatic</h1>
-      <Form />
-      {/* <form>
-        <h2 className="label-wrapper">
-          <label htmlFor="new-todo-input" className="label__lg">
-            What needs to be done?
-          </label>
-        </h2>
-        <input
-          type="text"
-          id="new-todo-input"
-          className="input input__lg"
-          name="text"
-          autoComplete="off"
-        />
-        <button type="submit" className="btn btn__primary btn__lg">
-          Add
-        </button>
-      </form> */}
+      <Form addTask={addTask} />
       <div className="filters btn-group stack-exception">
         <FilterButton />
         <FilterButton />
         <FilterButton />
-        {/* <button type="button" className="btn toggle-btn" aria-pressed="true">
-          <span className="visually-hidden">Show </span>
-          <span>all</span>
-          <span className="visually-hidden"> tasks</span>
-        </button>
-        <button type="button" className="btn toggle-btn" aria-pressed="false">
-          <span className="visually-hidden">Show </span>
-          <span>Active</span>
-          <span className="visually-hidden"> tasks</span>
-        </button>
-        <button type="button" className="btn toggle-btn" aria-pressed="false">
-          <span className="visually-hidden">Show </span>
-          <span>Completed</span>
-          <span className="visually-hidden"> tasks</span>
-        </button> */}
       </div>
       <h2 id="list-heading">
         3 tasks remaining
--- React/00001-React-お試しアプリ/my-app/src/components/Form.tsx
+++ React/00001-React-お試しアプリ/my-app/src/components/Form.tsx
@@ -1,25 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
+
 
 function Form(props:any) {
-  return (
-    <form>
-      <h2 className="label-wrapper">
-        <label htmlFor="new-todo-input" className="label__lg">
-          What needs to be done?
-        </label>
-      </h2>
-      <input
-        type="text"
-        id="new-todo-input"
-        className="input input__lg"
-        name="text"
-        autoComplete="off"
-      />
-      <button type="submit" className="btn btn__primary btn__lg">
-        Add
-      </button>
-    </form>
-  );
-}
+    
+    const [name, setName] = useState(""); //nameプロパティのデフォルト値を空文字に設定
+  
+    function handleChange(e:any) {
+      //テキストボックスに入力されたことをイベントとして検知する関数定義
+      //入力時の情報からテキストボックスのデータをReact側で状態管理
+      setName(e.target.value); //イベントを受け取ったら、ブラウザの入力情報をsetterで設定
+    }
+  
+    function handleSubmit(e:any) {
 
-export default Form;
+      //Addボタンをはじめとする、送信処理時にApp.tsx側で宣言した際の引数から、ポップアップイベントを実施
+
+      
+      e.preventDefault();//submitイベントのデフォルトの動きを抑止
+      props.addTask(name);////nameプロパティをタスクとして追加
+      setName("");//nameプロパティ初期化
+    }
+
+
+    //reactで管理する関数名ないしはプロパティ名は{}でくくる
+
+    return (
+      <form onSubmit={handleSubmit}>
+        <h2 className="label-wrapper">
+          <label htmlFor="new-todo-input" className="label__lg">
+            What needs to be done?
+          </label>
+        </h2>
+        <input
+          type="text"
+          id="new-todo-input"
+          className="input input__lg"
+          name="text"
+          autoComplete="off"
+          value={name}
+          onChange={handleChange}
+        />
+        <button type="submit" className="btn btn__primary btn__lg">
+          Add
+        </button>
+      </form>
+    );
+  }
+  
+  export default Form;
