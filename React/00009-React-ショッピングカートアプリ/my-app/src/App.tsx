c7f80d1709a7099047f29806ead49b7b2693affd
ukijumotahaneniarukenia <mrchildrenkh1008@gmail.com>
2020-07-22 01:13:33 +0900

    sketch

--- React/00009-React-ショッピングカートアプリ/my-app/src/App.tsx
+++ React/00009-React-ショッピングカートアプリ/my-app/src/App.tsx
@@ -5,25 +5,19 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App(){
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col-6">column number one</div>
+          <div className="col-6">
+            <span>
+              <i className="fas fa-home" />
+            </span>
+          </div>
+        </div>
+      </div>
+    );
 }
 
 export default App;
