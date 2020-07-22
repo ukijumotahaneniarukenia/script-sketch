9530c210ef7edbca77767c62e485b9c6ee9c145c
ukijumotahaneniarukenia <mrchildrenkh1008@gmail.com>
2020-07-22 09:42:18 +0900

    sketch

--- React/00009-React-ショッピングカートアプリ/my-app/src/components/NavBar.tsx
+++ React/00009-React-ショッピングカートアプリ/my-app/src/components/NavBar.tsx
@@ -6,13 +6,13 @@ import logo from '../logo.svg'
 
 export default function NavBar(){
     return (
-        <nav className="navbar navbar-expand-sm navbar-dark px-sm-5">
+        /* 
+        https://www.iconfinder.com/icons/1243689/call_phone_icon
+        Creative Commons (Attribution 3.0 Unported);
+        https://www.iconfinder.com/Makoto_msk */
+        <nav className="navbar navbar-expand-sm navbar-dark bg-primary px-sm-5">
 
             <Link to="/">
-                /* 
-                https://www.iconfinder.com/icons/1243689/call_phone_icon
-                Creative Commons (Attribution 3.0 Unported);
-                https://www.iconfinder.com/Makoto_msk */
                 <img src={logo} alt="store" className="navbar-brand" />
             </Link>
             
