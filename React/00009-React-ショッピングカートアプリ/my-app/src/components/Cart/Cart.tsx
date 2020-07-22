1de58b154f6ae4f34c2478656fcfdfe7eb7f516a
ukijumotahaneniarukenia <mrchildrenkh1008@gmail.com>
2020-07-23 05:05:59 +0900

    空カート画面の実装

--- React/00009-React-ショッピングカートアプリ/my-app/src/components/Cart/Cart.tsx
+++ React/00009-React-ショッピングカートアプリ/my-app/src/components/Cart/Cart.tsx
@@ -2,10 +2,34 @@ import React from 'react';
 
 import Title from '../Title'
 
+
+import CartColumns from './CartColumns'
+
+import EmptyCart from './EmptyCart'
+
+import {ProductConsumer} from '../../components/ProductContextWrapper'
+
+//Cartをrootとしたサブエントリ管理
 export default function Cart(){
     return (
-        <section>
-            <Title name="your" title="cart" />
-        </section>
+
+        <ProductConsumer>
+            {(context:any)=>{
+                const cart = context;
+
+                if(cart.length>0){
+                    return (
+                        <section>
+                            <Title name="your" title="cart" />
+                            <CartColumns />
+                        </section>
+                    )
+                }else{
+                    return <EmptyCart />
+                }
+
+            }}
+        </ProductConsumer>
+
     )
 }
