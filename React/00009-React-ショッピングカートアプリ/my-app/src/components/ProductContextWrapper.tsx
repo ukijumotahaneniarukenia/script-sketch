c4a91d69afc5f84c5a0446426307091f0069817d
ukijumotahaneniarukenia <mrchildrenkh1008@gmail.com>
2020-07-22 22:28:01 +0900

    詳細画面の見た目実装

--- React/00009-React-ショッピングカートアプリ/my-app/src/components/ProductContextWrapper.tsx
+++ React/00009-React-ショッピングカートアプリ/my-app/src/components/ProductContextWrapper.tsx
@@ -8,60 +8,44 @@ const ProductContext = React.createContext({}); //デフォルトをマップで
 //Provider
 //Consumer
 
-
-
 //コンテキストはクラス単位で管理する
 
 //クラスは状態を管理できるため
 
+//コンテキストはコンポーネント間で共通して持ちわまることができる
 class ProductProvider extends Component{
 
-
     state = {
-        storeProductList:storeProductListData
+        storeProductList:[]
         ,storeProductDetail:storeProductDetailData
     }
-
-
-    //アロー関数
-    //https://applingo.tokyo/article/1422
-    //アロー関数は自動的にthisの値をbindする
-    //アロー関数はES6から利用できる機能
-    //React開発環境ではES6に対応
-    //基本この書き方がいいぽい
-    testCheckStateBehavior = () => {
-        console.log("State Products :",this.state.storeProductList[0].inCart);//State Products : false
-        console.log("Data Products :",storeProductListData[0].inCart);//Data Products : false
-
-
-        const tempProducts = [...this.state.storeProductList];
-
-        tempProducts[0].inCart = true
-
-        //setState関数はReactオブジェクトだけでなく、データ側も更新する
-        this.setState(()=>{
-            return {storeProductList:tempProducts} //変更内容を反映
-        },()=>{
-            console.log("State Products :",this.state.storeProductList[0].inCart);//State Products : true
-            console.log("Data Products :",storeProductListData[0].inCart);//Data Products : true
-        })
-
-
+    
+    componentDidMount(){
+        this.setStoreProducts();
     }
-
-
-
-    hadleDetail() {
+    
+    setStoreProducts = ()=>{
+        let tempItemList : any[] =[]
+        storeProductListData.forEach(item=>{
+            const  singleItem = {...item}
+            tempItemList = [...tempItemList,singleItem]
+        });
+    
+        this.setState(()=>{
+            return {storeProductList:tempItemList}
+        });
+    }
+    
+    hadleDetail = () => {
         console.log("ハンドル詳細")    
     }
-
-    addToCart() {
-        console.log("かごに追加")    
+    
+    addToCart = (id:any) => {
+        console.log(`かごに追加 id:${id}`)    
     }
-
-
+    
     render(){
-
+    
         //データも関数も渡せる
         return (
             <ProductContext.Provider value={{
@@ -69,11 +53,11 @@ class ProductProvider extends Component{
                 ,hadleDetail:this.hadleDetail
                 ,addToCart:this.addToCart
             }}>
-                <button onClick={this.testCheckStateBehavior}>test state </button>
                 {this.props.children}
             </ProductContext.Provider>
         )
     }
+
 }
 
 const ProductConsumer = ProductContext.Consumer
