2e32e6f48a3be82bf209d35fd4795c7f4188bfaa
ukijumotahaneniarukenia <mrchildrenkh1008@gmail.com>
2020-08-02 04:15:35 +0900

    sketch

--- systemd_nspawn/systemd-container-network-setup-execute.sh
+++ systemd_nspawn/systemd-container-network-setup-execute.sh
@@ -2,10 +2,14 @@ cp -a /var/lib/machines/vir-ubuntu-20-04-template /var/lib/machines/vir-ubuntu-2
 cp -a /var/lib/machines/vir-ubuntu-20-04-template /var/lib/machines/vir-ubuntu-20-04-03
 cp -a /var/lib/machines/vir-ubuntu-20-04-template /var/lib/machines/vir-ubuntu-20-04-04
 cp -a /var/lib/machines/vir-ubuntu-20-04-template /var/lib/machines/vir-ubuntu-20-04-05
-machinectl shell root@vir-ubuntu-20-04-02 /bin/bash -c 'echo "root:root_pwd"|chpasswd'
-machinectl shell root@vir-ubuntu-20-04-03 /bin/bash -c 'echo "root:root_pwd"|chpasswd'
-machinectl shell root@vir-ubuntu-20-04-04 /bin/bash -c 'echo "root:root_pwd"|chpasswd'
-machinectl shell root@vir-ubuntu-20-04-05 /bin/bash -c 'echo "root:root_pwd"|chpasswd'
+machinectl start vir-ubuntu-20-04-02
+machinectl start vir-ubuntu-20-04-03
+machinectl start vir-ubuntu-20-04-04
+machinectl start vir-ubuntu-20-04-05
+machinectl shell root@vir-ubuntu-20-04-02 /bin/bash -c 'echo "root:root_pwd"|chpasswd';
+machinectl shell root@vir-ubuntu-20-04-03 /bin/bash -c 'echo "root:root_pwd"|chpasswd';
+machinectl shell root@vir-ubuntu-20-04-04 /bin/bash -c 'echo "root:root_pwd"|chpasswd';
+machinectl shell root@vir-ubuntu-20-04-05 /bin/bash -c 'echo "root:root_pwd"|chpasswd';
 machinectl shell root@vir-ubuntu-20-04-02 /usr/bin/ln -sf /dev/null /etc/systemd/network/80-container-host0.network
 machinectl shell root@vir-ubuntu-20-04-03 /usr/bin/ln -sf /dev/null /etc/systemd/network/80-container-host0.network
 machinectl shell root@vir-ubuntu-20-04-04 /usr/bin/ln -sf /dev/null /etc/systemd/network/80-container-host0.network
@@ -14,11 +18,27 @@ machinectl shell root@vir-ubuntu-20-04-02 /usr/bin/sed -i 's/#DNS/DNS=192.168.1.
 machinectl shell root@vir-ubuntu-20-04-03 /usr/bin/sed -i 's/#DNS/DNS=192.168.1.1/' /etc/systemd/resolved.conf
 machinectl shell root@vir-ubuntu-20-04-04 /usr/bin/sed -i 's/#DNS/DNS=192.168.1.1/' /etc/systemd/resolved.conf
 machinectl shell root@vir-ubuntu-20-04-05 /usr/bin/sed -i 's/#DNS/DNS=192.168.1.1/' /etc/systemd/resolved.conf
-machinectl shell root@vir-ubuntu-20-04-02 /usr/bin/cp /var/lib/machines/80-container-host0.network-192.168.1.210 /etc/systemd/network/80-container-host0.network
-machinectl shell root@vir-ubuntu-20-04-03 /usr/bin/cp /var/lib/machines/80-container-host0.network-192.168.1.211 /etc/systemd/network/80-container-host0.network
-machinectl shell root@vir-ubuntu-20-04-04 /usr/bin/cp /var/lib/machines/80-container-host0.network-192.168.1.212 /etc/systemd/network/80-container-host0.network
-machinectl shell root@vir-ubuntu-20-04-05 /usr/bin/cp /var/lib/machines/80-container-host0.network-192.168.1.213 /etc/systemd/network/80-container-host0.network
-machinectl shell root@vir-ubuntu-20-04-02 /usr/bin/cp /var/lib/machines/host0.network-192.168.1.210 /etc/systemd/network/host0.network
-machinectl shell root@vir-ubuntu-20-04-03 /usr/bin/cp /var/lib/machines/host0.network-192.168.1.211 /etc/systemd/network/host0.network
-machinectl shell root@vir-ubuntu-20-04-04 /usr/bin/cp /var/lib/machines/host0.network-192.168.1.212 /etc/systemd/network/host0.network
-machinectl shell root@vir-ubuntu-20-04-05 /usr/bin/cp /var/lib/machines/host0.network-192.168.1.213 /etc/systemd/network/host0.network
+cp 80-container-host0.network-192.168.1.210 vir-ubuntu-20-04-02/etc/systemd/network/80-container-host0.network
+cp 80-container-host0.network-192.168.1.211 vir-ubuntu-20-04-03/etc/systemd/network/80-container-host0.network
+cp 80-container-host0.network-192.168.1.212 vir-ubuntu-20-04-04/etc/systemd/network/80-container-host0.network
+cp 80-container-host0.network-192.168.1.213 vir-ubuntu-20-04-05/etc/systemd/network/80-container-host0.network
+cp host0.network-192.168.1.210 vir-ubuntu-20-04-02/etc/systemd/network/host0.network
+cp host0.network-192.168.1.211 vir-ubuntu-20-04-03/etc/systemd/network/host0.network
+cp host0.network-192.168.1.212 vir-ubuntu-20-04-04/etc/systemd/network/host0.network
+cp host0.network-192.168.1.213 vir-ubuntu-20-04-05/etc/systemd/network/host0.network
+machinectl shell root@vir-ubuntu-20-04-02 /bin/systemctl enable systemd-networkd
+machinectl shell root@vir-ubuntu-20-04-02 /bin/systemctl enable systemd-resolved
+machinectl shell root@vir-ubuntu-20-04-03 /bin/systemctl enable systemd-networkd
+machinectl shell root@vir-ubuntu-20-04-03 /bin/systemctl enable systemd-resolved
+machinectl shell root@vir-ubuntu-20-04-04 /bin/systemctl enable systemd-networkd
+machinectl shell root@vir-ubuntu-20-04-04 /bin/systemctl enable systemd-resolved
+machinectl shell root@vir-ubuntu-20-04-05 /bin/systemctl enable systemd-networkd
+machinectl shell root@vir-ubuntu-20-04-05 /bin/systemctl enable systemd-resolved
+machinectl shell root@vir-ubuntu-20-04-02 /bin/systemctl start systemd-networkd
+machinectl shell root@vir-ubuntu-20-04-02 /bin/systemctl start systemd-resolved
+machinectl shell root@vir-ubuntu-20-04-03 /bin/systemctl start systemd-networkd
+machinectl shell root@vir-ubuntu-20-04-03 /bin/systemctl start systemd-resolved
+machinectl shell root@vir-ubuntu-20-04-04 /bin/systemctl start systemd-networkd
+machinectl shell root@vir-ubuntu-20-04-04 /bin/systemctl start systemd-resolved
+machinectl shell root@vir-ubuntu-20-04-05 /bin/systemctl start systemd-networkd
+machinectl shell root@vir-ubuntu-20-04-05 /bin/systemctl start systemd-resolved
--- systemd_nspawn/systemd-container-network-setup.sh
+++ systemd_nspawn/systemd-container-network-setup.sh
@@ -23,6 +23,8 @@ HOST0_NETWORK_NAME=host0.network
 
 KEYWORDS=($CONTAINER_HOST0_NETWORK_NAME $HOST0_NETWORK_NAME)
 
+ACTION_LIST=(enable start)
+
 MY_ROUTER_IP=192.168.1.1
 
 GOOGLE_DNS_IP=8.8.8.8
@@ -32,33 +34,49 @@ RESOLVE_CONF_FILE_PATH=/etc/systemd/resolved.conf
 SEGMENT_THIRD_OCTET_IP=192.168.1
 SEGMENT_FORTH_OCTET_START_IP=209
 
-eval echo {$START_HOST_NO..$END_HOST_NO} | xargs -n1 | xargs -I@ echo cp -a $WORKDIR/$SEED_NAME $WORKDIR/$REPLICA_NAME-@
+{
+  eval echo {$START_HOST_NO..$END_HOST_NO} | xargs -n1 | xargs -I@ echo cp -a $WORKDIR/$SEED_NAME $WORKDIR/$REPLICA_NAME-@
 
-eval echo {$START_HOST_NO..$END_HOST_NO} | xargs -n1 | while read n;do printf "machinectl shell root@$REPLICA_NAME-%s /bin/bash -c \x27echo \x22root:root_pwd\x22|chpasswd\x27\n" $(printf "%02d" $n);done
+  eval echo {$START_HOST_NO..$END_HOST_NO} | xargs -n1 | while read n;do printf "machinectl start $REPLICA_NAME-%s\n" $(printf "%02d" $n);done
 
-eval echo {$START_HOST_NO..$END_HOST_NO} | xargs -n1 | xargs -I{} echo machinectl shell root@$REPLICA_NAME-{} /usr/bin/ln -sf /dev/null $DEPLOY_DIR/$CONTAINER_HOST0_NETWORK_NAME
+  #一旦ログインパスワードは一行ずつやさしく
+  #TODOなんとかしたい
+  eval echo {$START_HOST_NO..$END_HOST_NO} | xargs -n1 | while read n;do printf "machinectl shell root@$REPLICA_NAME-%s /bin/bash -c \x27echo \x22root:root_pwd\x22|chpasswd\x27;\n" $(printf "%02d" $n);done
 
-eval echo {$START_HOST_NO..$END_HOST_NO} | xargs -n1 | while read n;do printf "machinectl shell root@$REPLICA_NAME-%s /usr/bin/sed -i \x27s/#DNS/DNS=$MY_ROUTER_IP/\x27 $RESOLVE_CONF_FILE_PATH\n" $(printf "%02d" $n);done
+  eval echo {$START_HOST_NO..$END_HOST_NO} | xargs -n1 | xargs -I{} echo machinectl shell root@$REPLICA_NAME-{} /usr/bin/ln -sf /dev/null $DEPLOY_DIR/$CONTAINER_HOST0_NETWORK_NAME
 
-
-CNT=$(eval echo {$START_HOST_NO..$END_HOST_NO} | xargs -n1 | wc -l)
-
-seq $CNT | xargs -I@ echo echo $GOOGLE_DNS_IP $SEGMENT_THIRD_OCTET_IP.$\[$SEGMENT_FORTH_OCTET_START_IP+@\] $MY_ROUTER_IP|bash | \
-  while read google_dns_ip server_ip my_router_ip;do
-    sed "s/GOOGLE_DNS_IP/$google_dns_ip/;s/SERVER_IP/$server_ip/;s/MY_ROUTER_IP/$my_router_ip/;" $CONTAINER_HOST0_NETWORK_NAME >$WORKDIR/$CONTAINER_HOST0_NETWORK_NAME-$server_ip;
-  done
-
-seq $CNT | xargs -I@ echo echo $SEGMENT_THIRD_OCTET_IP.$\[$SEGMENT_FORTH_OCTET_START_IP+@\] $MY_ROUTER_IP |bash|\
-  while read server_ip my_router_ip;do
-    sed "s/SERVER_IP/$server_ip/;s/MY_ROUTER_IP/$my_router_ip/;" $HOST0_NETWORK_NAME >$HOST0_NETWORK_NAME-$server_ip;
-  done
+  eval echo {$START_HOST_NO..$END_HOST_NO} | xargs -n1 | while read n;do printf "machinectl shell root@$REPLICA_NAME-%s /usr/bin/sed -i \x27s/#DNS/DNS=$MY_ROUTER_IP/\x27 $RESOLVE_CONF_FILE_PATH\n" $(printf "%02d" $n);done
 
 
-for keyword in ${KEYWORDS[@]} ;do
+  CNT=$(eval echo {$START_HOST_NO..$END_HOST_NO} | xargs -n1 | wc -l)
 
-  eval echo {$START_HOST_NO..$END_HOST_NO} | xargs -n1|nl|\
-    while read n h;do
-      echo machinectl shell root@$REPLICA_NAME-$(printf "%02d" $[n+1]) /usr/bin/cp $WORKDIR/$(ls $keyword* | grep $SEGMENT_THIRD_OCTET_IP.$[$SEGMENT_FORTH_OCTET_START_IP+$n]) $DEPLOY_DIR/$keyword;
+  seq $CNT | xargs -I@ echo echo $GOOGLE_DNS_IP $SEGMENT_THIRD_OCTET_IP.$\[$SEGMENT_FORTH_OCTET_START_IP+@\] $MY_ROUTER_IP|bash | \
+    while read google_dns_ip server_ip my_router_ip;do
+      sed "s/GOOGLE_DNS_IP/$google_dns_ip/;s/SERVER_IP/$server_ip/;s/MY_ROUTER_IP/$my_router_ip/;" $CONTAINER_HOST0_NETWORK_NAME >$WORKDIR/$CONTAINER_HOST0_NETWORK_NAME-$server_ip;
     done
 
-done
+  seq $CNT | xargs -I@ echo echo $SEGMENT_THIRD_OCTET_IP.$\[$SEGMENT_FORTH_OCTET_START_IP+@\] $MY_ROUTER_IP |bash|\
+    while read server_ip my_router_ip;do
+      sed "s/SERVER_IP/$server_ip/;s/MY_ROUTER_IP/$my_router_ip/;" $HOST0_NETWORK_NAME >$HOST0_NETWORK_NAME-$server_ip;
+    done
+
+
+  for keyword in ${KEYWORDS[@]} ;do
+
+    eval echo {$START_HOST_NO..$END_HOST_NO} | xargs -n1|nl|\
+      while read n h;do
+        echo cp $(ls $keyword* | grep $SEGMENT_THIRD_OCTET_IP.$[$SEGMENT_FORTH_OCTET_START_IP+$n]) $REPLICA_NAME-$(printf "%02d" $[n+1])$DEPLOY_DIR/$keyword;
+      done
+
+  done
+
+  for action in ${ACTION_LIST[@]} ;do
+
+    eval echo {$START_HOST_NO..$END_HOST_NO} | xargs -n1|nl|\
+      while read n h;do
+        echo machinectl shell root@$REPLICA_NAME-$(printf "%02d" $[n+1]) /bin/systemctl $action systemd-networkd
+        echo machinectl shell root@$REPLICA_NAME-$(printf "%02d" $[n+1]) /bin/systemctl $action systemd-resolved
+      done
+
+  done
+} #| bash
