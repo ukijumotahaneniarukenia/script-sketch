49f93e0992e12772946d9270603af73cf1c446e2
ukijumotahaneniarukenia <mrchildrenkh1008@gmail.com>
2020-08-07 02:17:55 +0900

    sketch

--- ruby/00004-ruby-日本語ダミデータ作成.rb
+++ ruby/00004-ruby-日本語ダミデータ作成.rb
@@ -1,32 +1,53 @@
 #!/usr/bin/env ruby
 
-require 'faker'
+#ロケール抜くやつ
+#$ find -type f | grep lib | grep locale | grep -v address | while read f;do grep -q address: $f;[ $? -eq 0 ]&&echo $f;done | ruby -F/ -anle 'puts $F[$F.size-1].sub(/\.yml/,"")'|sort |uniq|xargs
 
-Faker::Config.locale=:ja
+
+#https://github.com/faker-ruby/faker
+require 'faker'
 
 def usage
   f = File.basename(__FILE__)
   puts <<~EOF
 Usage:
-  IN : seq 10 | #{f} or #{f} 2
+
+LOCALE:ar bg ca-CAT da-DK de de-AT de-CH ee en-AU en-CA en-GB en-IND en-MS en-NEP en-NG en-NZ en-PAK en-SG en-UG en-US en-ZA en-au-ocker es es-MX fi-FI fr fr-CA fr-CH he hy id it ja ko lv nb-NO nl pl pt pt-BR ru sk sv tr uk vi zh-CN zh-TW
+
+  IN : echo en-US 20 | #{f}
+  OUT:
+
+  or
+
+  IN : #{f} ja 20
   OUT:
 EOF
   return 0
 end
 
-def mock(ary)
-  ary.map do|e|
-    puts Faker::Lorem.sentence(word_count:e.to_i)
+def generate(ary)
+  Faker::Config.locale=ary[0]
+
+  [*1..ary[1].to_i].map do|e|
+    puts Faker::Name.name#氏名
+    # puts Faker::Internet::HTTP.status_code(group: :information)
   end
 end
 
 if ARGV.length > 0
   cmd_args = [ *ARGV ]
-  args = cmd_args[0].split(/\n/)
-  mock(args)
+
+  if cmd_args.size != 2
+    usage
+  end
+
+  generate(cmd_args)
+
 elsif FileTest.pipe?(STDIN)
-  pipe_args = STDIN.readlines.map { |line| line.chomp }
-  mock(pipe_args)
+
+  pipe_args = STDIN.readlines.map{|e|e.split(/\s+/)}.flatten
+
+  generate(pipe_args)
 else
   usage
 end
