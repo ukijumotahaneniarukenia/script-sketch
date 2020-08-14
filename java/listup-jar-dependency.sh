883332b6d0844656236d39affbafa5ff8b2c5e72
ukijumotahaneniarukenia <mrchildrenkh1008@gmail.com>
2020-08-14 20:35:58 +0900

    sketch

--- java/listup-jar-dependency.sh
+++ java/listup-jar-dependency.sh
@@ -2,6 +2,26 @@
 
 WORKDIR=$HOME/script-sketch/java/wrksp
 
-ls $WORKDIR/*tsv | xargs -I@ grep -P '/project/dependencies/dependency\[.*\]/artifactId|/project/dependencies/dependency\[.*\]/version' @ | grep -P '\[2\]' | \
-  awk '/artifactId/{print 1,$0};/version/{print 0,$0};' | awk '{print pre=pre+$1,$0;}'| ruby -anle 'p $F[0],$F[1],$F[2].gsub(/.*\//,""),$F[3]' | xargs -n4  | \
-  awk '{a[$1]=a[$1]$0}END{for(e in a)print e,a[e]}'|awk '{print $1,$4,$5,$7,$8}' OFS="\t" | sort -nk1 >listup-jar-dependency.tsv
+OUTPUT_FILE_NAME=listup-jar-dependency.tsv
+
+PATTERN="/project/dependencies/dependency\[.*\]/artifactId|/project/dependencies/dependency\[.*\]/version"
+
+START_NO=$(ls $WORKDIR/*tsv | xargs -I@ grep -P "$PATTERN" @  | grep -Po '(?<=\[)\d+(?=\])' | sort -nk1 | head -n1)
+
+END_NO=$(ls $WORKDIR/*tsv | xargs -I@ grep -P "$PATTERN" @  | grep -Po '(?<=\[)\d+(?=\])' | sort -nk1 | tail -n1)
+
+rm -rf $OUTPUT_FILE_NAME
+
+touch $OUTPUT_FILE_NAME
+
+for (( i=$START_NO;i<=$END_NO;i++ ));do
+
+  ls $WORKDIR/*tsv | xargs -I@ grep -P "$PATTERN" @ | grep -P "\[$i\]" | \
+    awk '/artifactId/{print 1,$0};/version/{print 0,$0};' | \
+    awk '{print pre=pre+$1,$0;}'| \
+    ruby -anle 'p $F[0],$F[1],$F[2].gsub(/.*\//,""),$F[3]' | xargs -n4  | \
+    awk '{key=$1;$1="";$2="";a[key]=a[key]$0}END{for(e in a)print e,a[e]}' | \
+    awk '{if (length($4)==0 && length($5)==0){print $1,$2,$3,"-","-"}else{print $1,$2,$3,$4,$5}}' OFS="\t" | \
+    awk '{$1="";print $0}' OFS="\t" | sed 's/^\t//' | sort | uniq >>$OUTPUT_FILE_NAME
+
+done
