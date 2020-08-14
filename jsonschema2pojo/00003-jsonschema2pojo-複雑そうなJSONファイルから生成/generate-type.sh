e26fec49ce00462085047b8cf7e104a3c584ae0b
ukijumotahaneniarukenia <mrchildrenkh1008@gmail.com>
2020-08-14 12:02:42 +0900

    sketch

--- jsonschema2pojo/00003-jsonschema2pojo-複雑そうなJSONファイルから生成/generate-type.sh
+++ jsonschema2pojo/00003-jsonschema2pojo-複雑そうなJSONファイルから生成/generate-type.sh
@@ -11,4 +11,6 @@ cat $INPUT_FILE_NAME | jq --stream -c -r '.[0]|map(if type=="number" then "["+to
     cat $INPUT_FILE_NAME | jq -r "[\"$path\",($path|type)]|join(\"\t\")"
 
 
-  done | teip -Gog '(?<=\[)\d+(?=\])' -- sed 's/./0/g' | sort | uniq | jq -R './"\t"|{(.[0]):{ "type":.[1],"format":""}}' | jq -s 'reduce .[] as $item ({};.+$item)'
+  done | teip -Gog '(?<=\[)\d+(?=\])' -- sed 's/./0/g' | sort | uniq | ruby -F"\t" -anle '$F<<case when $F[0].match(/(date|time)$/);"date-time";else "";end;puts $F.join("\t")' | \
+
+    jq -R './"\t"|{(.[0]):{ "type":.[1],"format":.[2]}}' | jq -s 'reduce .[] as $item ({};.+$item)'
