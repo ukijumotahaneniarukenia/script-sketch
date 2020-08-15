4921a47cc39d15ed344b15bd24a627998cff459d
ukijumotahaneniarukenia <mrchildrenkh1008@gmail.com>
2020-08-15 23:52:21 +0900

    sketch

--- java/00011-java-fakerでダミーデータ作成/nnn/src/main/java/app/App.java
+++ java/00011-java-fakerでダミーデータ作成/nnn/src/main/java/app/App.java
@@ -1,5 +1,7 @@
 package app;
 
+import com.github.javafaker.Faker;
+import com.github.javafaker.PrincessBride;
 import sun.misc.Signal;
 
 import java.util.*;
@@ -10,6 +12,7 @@ public class App {
     private static final String SIGNAL_HANDLE_INT = "INT";
 
     public static void main( String... cmdLineArgs ) {
+
         trap(new ArrayList<>(Arrays.asList(SIGNAL_HANDLE_INT)));
 
         if(cmdLineArgs.length > 0){
@@ -18,16 +21,30 @@ public class App {
             Map<Integer, List<String>> map = sub_process(pre_process(new Scanner(System.in)));
 
             if(!isSingleRow(map)){
-                //単一行チェック
                 usage();
             }
 
-            System.out.println(map);
+            if(!isTwoColumn(map)){
+                usage();
+            }
+
+            wrapperExecute(map);
 
         }
     }
+    private static void wrapperExecute(Map<Integer, List<String>> map){
+        Faker faker = new Faker(new Locale(map.get(0).get(0)));
+        PrincessBride princessBride = faker.princessBride();
+        int cnt = Integer.parseInt(map.get(0).get(1));
+        for(int i=0;i<cnt;i++){
+            System.out.println(princessBride.quote());
+        }
+    }
     private static boolean isSingleRow(Map<Integer, List<String>> map){
-        return map.size() != 1;
+        return map.size() == 1;
+    }
+    private static boolean isTwoColumn(Map<Integer, List<String>> map){
+        return map.get(0).size() == 2;
     }
     private static void trap(List<String> list){
         for (String e:list) {
@@ -44,6 +61,11 @@ public class App {
     }
     private static void usage() {
         System.out.println("Usageだよーん");
+        System.out.println("\n" +
+                "" +
+                "CMD: echo ja_JP 10 | java -jar /home/aine/script-sketch/java/00011-java-fakerでダミーデータ作成/nnn/target/TOBE_REPLACE_APP_NAME-1.0-SNAPSHOT-jar-with-dependencies.jar" +
+                "" +
+                "\n");
         System.exit(0);
     }
     private static List<String> pre_process(Scanner stdin){
