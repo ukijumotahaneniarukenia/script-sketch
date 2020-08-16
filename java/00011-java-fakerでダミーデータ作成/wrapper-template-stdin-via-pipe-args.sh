4af4f978f4caf2b07aa8f9476d443c6b7ee709c2
ukijumotahaneniarukenia <mrchildrenkh1008@gmail.com>
2020-08-16 23:09:07 +0900

    sketch

--- java/00011-java-fakerでダミーデータ作成/wrapper-template-stdin-via-pipe-args.sh
+++ java/00011-java-fakerでダミーデータ作成/wrapper-template-stdin-via-pipe-args.sh
@@ -6,6 +6,28 @@ cat <<EOS
 
 Usage:
 
+LOCALE:
+
+       a ar
+       b bg by
+       c ca ca-CAT cs-CZ
+       d da-DK de de-AT de-CH
+       e ee en en-AU en-BORK en-CA en-GB en-IND en-MS en-NEP en-NG en-NZ en-PAK en-SG en-UG en-US en-ZA en-au-ocker es es-MX
+       f fa fi-FI fr fr-CA fr-CH
+       h he hu hy
+       i id in-ID it
+       j ja
+       k ko
+       l lv
+       n nb-NO nl no-NO
+       p pl pt pt-BR
+       r ru
+       s sk sv sv-SE
+       t th tr
+       u uk
+       v vi
+       z zh-CN zh-TW
+
   CMD: echo TOBE_REPLACE_INPUT_ARGS | ${0##*/}
 
 EOS
