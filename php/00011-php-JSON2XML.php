0487cdb0de61c1a22bc0d504feb2ccccd14d9279
ukijumotahaneniarukenia <mrchildrenkh1008@gmail.com>
2020-08-21 10:46:27 +0900

    sketch

--- php/00011-php-JSON2XML.php
+++ php/00011-php-JSON2XML.php
@@ -1,24 +1,17 @@
 <?php
 
+#エラーレベルのログのみ出力するように制御
+error_reporting(E_ERROR);
+
 $json_txt = '{"project":{"modelVersion":"4.0.0","groupId":"app","artifactId":"nnn","version":"0.1.0","parent":{"groupId":"org.springframework.boot","artifactId":"spring-boot-starter-parent","version":"2.2.4.RELEASE","relativePath":null},"dependencies":{"dependency":[{"groupId":"org.springframework.boot","artifactId":"spring-boot-starter-webflux"},{"groupId":"org.springframework.boot","artifactId":"spring-boot-starter-test","scope":"test"},{"groupId":"io.projectreactor","artifactId":"reactor-test","scope":"test"}]},"properties":{"java.version":"1.8"},"build":{"plugins":{"plugin":{"groupId":"org.springframework.boot","artifactId":"spring-boot-maven-plugin"}}}}}';
 
-$ary = (Array) json_decode($json_txt);
-
-$ary2 = json_decode($json_txt,true);
+$ary = json_decode($json_txt,true);
 
 $xml = new SimpleXMLElement('<root/>');
 
-print_r($ary2);
+print_r ($ary);
 
-#array_walk_recursive($ary, array ($xml, 'addChild'));
+#関数の部分適用時、引数に与えた関数が引数を必要としている場合、実行時のパーサがないと判断するので、ワーニングログを出力をはく
+array_walk_recursive($ary, array ($xml, 'addChild'));
 
-
-#print $xml->asXML();
-
-
-
-
-
-
-
-#print_r ( $aryobj);
+print $xml->asXML();
