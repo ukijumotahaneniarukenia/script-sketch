49429fdf1398fcf76e9df5363f0b21ae39cfc03c
ukijumotahaneniarukenia <mrchildrenkh1008@gmail.com>
2020-08-27 21:34:48 +0900

    sketch

--- csharp/00016-csharp-動的にインスタンスを生成外部ライブラリあり/app/Program.cs
+++ csharp/00016-csharp-動的にインスタンスを生成外部ライブラリあり/app/Program.cs
@@ -11,13 +11,199 @@ using Newtonsoft.Json;
 namespace app {
     class Program {
 
-        private const string ASSEMBLY_NAME = "assemblyName";
-        private const string FS = "\t";
+        private static string EMPTY = "";
+        private static string SEPARATOR = " ";
+        private static string FS = "\t";
+        private static char RS = '\n';
+        private static string STRING_JOINER = ",";
+        private static string COLUMN_JOINER = "-";
+        private static string GROUP_DIGIT = "{0:00000000}";
+        private static string SUB_GROUP_DIGIT = "{0:000000}";
+        private static string GROUP_SEQ_DIGIT = "{0:0000}";
+        private static string GROUP = "グループ番号";
+        private static string GROUP_SEQ = "グループシーケンス番号";
+        private const string ASSEMBLY_NAME = "アセンブリ名";
+        private const string NAMESPACE_NAME = "名前空間名";
+        private static string TYPE_NAME = "型名";
+        private static List<string> OUTPUT_COMMON_HEADER_LIST = new List<string> {
+            GROUP,
+            ASSEMBLY_NAME,
+            NAMESPACE_NAME,
+            TYPE_NAME,
+            GROUP_SEQ
+        };
+        private static string PROPERTY_OF_STATIC = "スタティックプロパティ名";
+        private static string PROPERTY_OF_STATIC_RETURN_TYPE_NAME = "スタティックプロパティ名の戻り値の型名";
+        private static List<string> OUTPUT_STATIC_PROPERTY_HEADER_LIST = new List<string> {
+            PROPERTY_OF_STATIC,
+            PROPERTY_OF_STATIC_RETURN_TYPE_NAME
+        };
+        private static string PROPERTY_OF_INSTANCE = "インスタンスプロパティ名";
+        private static string PROPERTY_OF_INSTANCE_RETURN_TYPE_NAME = "インスタンスプロパティ名の戻り値の型名";
+        private static List<string> OUTPUT_INSTANCE_PROPERTY_HEADER_LIST = new List<string> {
+            PROPERTY_OF_INSTANCE,
+            PROPERTY_OF_INSTANCE_RETURN_TYPE_NAME
+        };
+        private static string METHOD_OF_STATIC_NAME = "スタティックメソッド名";
+        private static string METHOD_OF_STATIC_RETURN_TYPE_NAME = "スタティックメソッドの戻り値の型名";
+        private static string METHOD_OF_STATIC_PHONY_ARGUMENT_COUNT = "スタティックメソッドの仮引数の個数";
+        private static string METHOD_OF_STATIC_PHONY_ARGUMENT_POSITION_NO = "スタティックメソッドの仮引数の位置番号";
+        private static string METHOD_OF_STATIC_PHONY_ARGUMENT_VARIABLE_NAME = "スタティックメソッドの仮引数の変数名";
+        private static string METHOD_OF_STATIC_PHONY_ARGUMENT_RETURN_TYPE_NAME = "スタティックメソッドの仮引数の型名";
+        private static List<string> OUTPUT_STATIC_METHOD_HEADER_LIST = new List<string> {
+            METHOD_OF_STATIC_NAME,
+            METHOD_OF_STATIC_RETURN_TYPE_NAME,
+            METHOD_OF_STATIC_PHONY_ARGUMENT_COUNT,
+            METHOD_OF_STATIC_PHONY_ARGUMENT_POSITION_NO,
+            METHOD_OF_STATIC_PHONY_ARGUMENT_VARIABLE_NAME,
+            METHOD_OF_STATIC_PHONY_ARGUMENT_RETURN_TYPE_NAME
+        };
+        private static string METHOD_OF_INSTANCE_NAME = "インスタンスメソッド名";
+        private static string METHOD_OF_INSTANCE_RETURN_TYPE_NAME = "インスタンスメソッドの戻り値の型名";
+        private static string METHOD_OF_INSTANCE_PHONY_ARGUMENT_COUNT = "インスタンスメソッドの仮引数の個数";
+        private static string METHOD_OF_INSTANCE_PHONY_ARGUMENT_POSITION_NO = "インスタンスメソッドの仮引数の位置番号";
+        private static string METHOD_OF_INSTANCE_PHONY_ARGUMENT_VARIABLE_NAME = "インスタンスメソッドの仮引数の変数名";
+        private static string METHOD_OF_INSTANCE_PHONY_ARGUMENT_RETURN_TYPE_NAME = "インスタンスメソッドの仮引数の型名";
+        private static List<string> OUTPUT_INSTANCE_METHOD_HEADER_LIST = new List<string> {
+            METHOD_OF_INSTANCE_NAME,
+            METHOD_OF_INSTANCE_RETURN_TYPE_NAME,
+            METHOD_OF_INSTANCE_PHONY_ARGUMENT_COUNT,
+            METHOD_OF_INSTANCE_PHONY_ARGUMENT_POSITION_NO,
+            METHOD_OF_INSTANCE_PHONY_ARGUMENT_VARIABLE_NAME,
+            METHOD_OF_INSTANCE_PHONY_ARGUMENT_RETURN_TYPE_NAME
+        };
         private const string DEFAULT_NONE_STRING_VALUE = "ないよーん";
-        private const string NAMESPACE_NAME = "nameSpaceName";
-        private const string TYPE_NAME = "typeName";
+        private static int DEFAULT_NONE_INT_VALUE = 0;
+        private static List<string> DEFAULT_OUTPUT_HEADER_LIST = OUTPUT_INSTANCE_PROPERTY_HEADER_LIST;
+        private static string DEFAULT_OPTION_VALUE = "--property-instance";
+        private const string OPTION_PROPERTY_INSTANCE = "--property-instance";
+        private const string OPTION_PROPERTY_STATIC = "--property-static";
+        private const string OPTION_METHOD_INSTANCE = "--method-instance";
+        private const string OPTION_METHOD_STATIC = "--method-static";
 
-        private static List<Dictionary<string, string>> getStdLibAssemblyList () {
+        //クラスのパブリックなスタティックプロパティを取得
+        private static Dictionary<string, Dictionary<string, string>> getPropertyOfStaticSummaryInfoDict (Type type) {
+            Dictionary<string, Dictionary<string, string>> propertyOfStaticSummaryInfoDict = new Dictionary<string, Dictionary<string, string>> ();
+
+            int subgrp = 0;
+
+            List<PropertyInfo> propertyInfoList = type.GetProperties (BindingFlags.Static | BindingFlags.Public).ToList ();
+
+            foreach (PropertyInfo propertyInfo in propertyInfoList) {
+
+                subgrp++;
+
+                Dictionary<string, string> propertyOfStaticDetailInfoDict = new Dictionary<string, string> ();
+
+                propertyOfStaticDetailInfoDict.Add (PROPERTY_OF_INSTANCE, propertyInfo.Name); //スタティックプロパティ名
+                propertyOfStaticDetailInfoDict.Add (PROPERTY_OF_INSTANCE_RETURN_TYPE_NAME, propertyInfo.ToString ().Split (SEPARATOR) [0]); //スタティックプロパティ名の戻り値の型名
+
+                propertyOfStaticSummaryInfoDict.Add (String.Format (SUB_GROUP_DIGIT, subgrp) + COLUMN_JOINER + type.FullName, propertyOfStaticDetailInfoDict);
+            }
+
+            return propertyOfStaticSummaryInfoDict;
+        }
+
+        //クラスのパブリックなインスタンスプロパティを取得
+        private static Dictionary<string, Dictionary<string, string>> getPropertyOfInstanceSummaryInfoDict (Type type) {
+            Dictionary<string, Dictionary<string, string>> propertyOfInstanceSummaryInfoDict = new Dictionary<string, Dictionary<string, string>> ();
+
+            int subgrp = 0;
+
+            List<PropertyInfo> propertyInfoList = type.GetProperties (BindingFlags.Instance | BindingFlags.Public).ToList ();
+
+            foreach (PropertyInfo propertyInfo in propertyInfoList) {
+
+                subgrp++;
+
+                Dictionary<string, string> propertyOfInstanceDetailInfoDict = new Dictionary<string, string> ();
+
+                propertyOfInstanceDetailInfoDict.Add (PROPERTY_OF_STATIC, propertyInfo.Name); //インスタンスプロパティ名
+                propertyOfInstanceDetailInfoDict.Add (PROPERTY_OF_STATIC_RETURN_TYPE_NAME, propertyInfo.ToString ().Split (SEPARATOR) [0]); //インスタンスプロパティ名の戻り値の型名
+
+                propertyOfInstanceSummaryInfoDict.Add (String.Format (SUB_GROUP_DIGIT, subgrp) + COLUMN_JOINER + type.FullName, propertyOfInstanceDetailInfoDict);
+            }
+
+            return propertyOfInstanceSummaryInfoDict;
+
+        }
+
+        // クラスのパブリックなスタティックメソッドを取得
+        private static Dictionary<string, Dictionary<string, string>> getMethodOfStaticSummaryInfoDict (Type type) {
+            Dictionary<string, Dictionary<string, string>> methodOfStaticSummaryInfoDict = new Dictionary<string, Dictionary<string, string>> ();
+
+            int subgrp = 0;
+
+            List<MethodInfo> methodOfStaticInfoList = type.GetMethods (BindingFlags.Static | BindingFlags.Public).ToList ();
+
+            foreach (MethodInfo methodOfStaticInfo in methodOfStaticInfoList) {
+
+                subgrp++;
+
+                List<ParameterInfo> parameterOfStaticInfoList = methodOfStaticInfo.GetParameters ().ToList ();
+
+                Dictionary<string, string> methodOfStaticDetailInfoDict = new Dictionary<string, string> ();
+
+                methodOfStaticDetailInfoDict.Add (METHOD_OF_STATIC_NAME, methodOfStaticInfo.Name); //メソッド名
+                methodOfStaticDetailInfoDict.Add (METHOD_OF_STATIC_RETURN_TYPE_NAME, methodOfStaticInfo.ReturnType.FullName); //メソッドの戻り値の型名
+
+                if (parameterOfStaticInfoList.Count == 0) {
+                    methodOfStaticDetailInfoDict.Add (METHOD_OF_STATIC_PHONY_ARGUMENT_COUNT, DEFAULT_NONE_INT_VALUE.ToString ()); //メソッドの仮引数の個数
+                    methodOfStaticDetailInfoDict.Add (METHOD_OF_STATIC_PHONY_ARGUMENT_POSITION_NO, DEFAULT_NONE_STRING_VALUE); //メソッドの仮引数の位置番号
+                    methodOfStaticDetailInfoDict.Add (METHOD_OF_STATIC_PHONY_ARGUMENT_VARIABLE_NAME, DEFAULT_NONE_STRING_VALUE); //メソッドの仮引数の変数名
+                    methodOfStaticDetailInfoDict.Add (METHOD_OF_STATIC_PHONY_ARGUMENT_RETURN_TYPE_NAME, DEFAULT_NONE_STRING_VALUE); //メソッドの仮引数の型名
+                } else {
+                    methodOfStaticDetailInfoDict.Add (METHOD_OF_STATIC_PHONY_ARGUMENT_COUNT, string.Join (STRING_JOINER, parameterOfStaticInfoList.Count.ToString ())); //メソッドの仮引数の個数
+                    methodOfStaticDetailInfoDict.Add (METHOD_OF_STATIC_PHONY_ARGUMENT_POSITION_NO, string.Join (STRING_JOINER, parameterOfStaticInfoList.Select (parameterInfo => parameterInfo.Position).ToArray ())); //メソッドの仮引数の位置番号
+                    methodOfStaticDetailInfoDict.Add (METHOD_OF_STATIC_PHONY_ARGUMENT_VARIABLE_NAME, string.Join (STRING_JOINER, parameterOfStaticInfoList.Select (parameterInfo => parameterInfo.Name).ToArray ())); //メソッドの仮引数の変数名
+                    methodOfStaticDetailInfoDict.Add (METHOD_OF_STATIC_PHONY_ARGUMENT_RETURN_TYPE_NAME, string.Join (STRING_JOINER, parameterOfStaticInfoList.Select (parameterInfo => parameterInfo.ParameterType.Name).ToArray ())); //メソッドの仮引数の型名
+                }
+
+                methodOfStaticSummaryInfoDict.Add (String.Format (SUB_GROUP_DIGIT, subgrp) + COLUMN_JOINER + type.FullName, methodOfStaticDetailInfoDict);
+            }
+
+            return methodOfStaticSummaryInfoDict;
+
+        }
+
+        // クラスのパブリックなインスタンスメソッドを取得
+        private static Dictionary<string, Dictionary<string, string>> getMethodOfInstanceSummaryInfoDict (Type type) {
+            Dictionary<string, Dictionary<string, string>> methodOfInstanceSummaryInfoDict = new Dictionary<string, Dictionary<string, string>> ();
+
+            int subgrp = 0;
+
+            List<MethodInfo> methodOfInstanceInfoList = type.GetMethods (BindingFlags.Instance | BindingFlags.Public).ToList ();
+
+            foreach (MethodInfo methodOfInstanceInfo in methodOfInstanceInfoList) {
+
+                subgrp++;
+
+                List<ParameterInfo> parameterOfInstanceInfoList = methodOfInstanceInfo.GetParameters ().ToList ();
+
+                Dictionary<string, string> methodOfInstanceDetailInfoDict = new Dictionary<string, string> ();
+
+                methodOfInstanceDetailInfoDict.Add (METHOD_OF_INSTANCE_NAME, methodOfInstanceInfo.Name); //メソッド名
+                methodOfInstanceDetailInfoDict.Add (METHOD_OF_INSTANCE_RETURN_TYPE_NAME, methodOfInstanceInfo.ReturnType.FullName); //メソッドの戻り値の型名
+
+                if (parameterOfInstanceInfoList.Count == 0) {
+                    methodOfInstanceDetailInfoDict.Add (METHOD_OF_INSTANCE_PHONY_ARGUMENT_COUNT, DEFAULT_NONE_INT_VALUE.ToString ()); //メソッドの仮引数の個数
+                    methodOfInstanceDetailInfoDict.Add (METHOD_OF_INSTANCE_PHONY_ARGUMENT_POSITION_NO, DEFAULT_NONE_STRING_VALUE); //メソッドの仮引数の位置番号
+                    methodOfInstanceDetailInfoDict.Add (METHOD_OF_INSTANCE_PHONY_ARGUMENT_VARIABLE_NAME, DEFAULT_NONE_STRING_VALUE); //メソッドの仮引数の変数名
+                    methodOfInstanceDetailInfoDict.Add (METHOD_OF_INSTANCE_PHONY_ARGUMENT_RETURN_TYPE_NAME, DEFAULT_NONE_STRING_VALUE); //メソッドの仮引数の型名
+                } else {
+                    methodOfInstanceDetailInfoDict.Add (METHOD_OF_INSTANCE_PHONY_ARGUMENT_COUNT, string.Join (STRING_JOINER, parameterOfInstanceInfoList.Count.ToString ())); //メソッドの仮引数の個数
+                    methodOfInstanceDetailInfoDict.Add (METHOD_OF_INSTANCE_PHONY_ARGUMENT_POSITION_NO, string.Join (STRING_JOINER, parameterOfInstanceInfoList.Select (parameterInfo => parameterInfo.Position).ToArray ())); //メソッドの仮引数の位置番号
+                    methodOfInstanceDetailInfoDict.Add (METHOD_OF_INSTANCE_PHONY_ARGUMENT_VARIABLE_NAME, string.Join (STRING_JOINER, parameterOfInstanceInfoList.Select (parameterInfo => parameterInfo.Name).ToArray ())); //メソッドの仮引数の変数名
+                    methodOfInstanceDetailInfoDict.Add (METHOD_OF_INSTANCE_PHONY_ARGUMENT_RETURN_TYPE_NAME, string.Join (STRING_JOINER, parameterOfInstanceInfoList.Select (parameterInfo => parameterInfo.ParameterType.Name).ToArray ())); //メソッドの仮引数の型名
+                }
+
+                methodOfInstanceSummaryInfoDict.Add (String.Format (SUB_GROUP_DIGIT, subgrp) + COLUMN_JOINER + type.FullName, methodOfInstanceDetailInfoDict);
+            }
+
+            return methodOfInstanceSummaryInfoDict;
+
+        }
+        private static List<Dictionary<string, string>> getStdLibAssemblyList (string appName) {
             //デフォルトの標準ライブラリのみ
             List<Assembly> stdlibAssemblyList = AppDomain.CurrentDomain.GetAssemblies ().ToList ();
 
@@ -69,38 +255,62 @@ namespace app {
             return assemblyNameSpaceTypeSummaryList;
         }
 
+        private static void Usage (string appName) {
+            Console.WriteLine (EMPTY +
+                // "\nOption:" + String.Join (SEPARATOR, OPTION_LIST.ToArray ()) +
+                RS +
+                "\nUsage:" +
+                RS +
+                // "\n  CMD: " + appName + SEPARATOR + USAGE_SAMPLE_ARGUMENT +
+                RS +
+                RS
+            );
+
+            Environment.Exit (0);
+        }
+
         static void Main (string[] args) {
 
-            List<Dictionary<string, string>> assemblyNameSpaceTypeSummaryList = getStdLibAssemblyList();
+            string appName = Environment.GetCommandLineArgs () [0];
+            appName = Regex.Replace (appName, ".*/", EMPTY); //ファイル名のみにする
+            appName = Regex.Replace (appName, "\\..*", EMPTY); //拡張子を取り除く
 
-            foreach (Dictionary<string, string> assemblyNameSpaceTypeDetailDict in assemblyNameSpaceTypeSummaryList)
-            {
-                {
-                    Console.Write(assemblyNameSpaceTypeDetailDict[ASSEMBLY_NAME]);
-                    Console.Write(FS);
-                    Console.Write(assemblyNameSpaceTypeDetailDict[NAMESPACE_NAME]);
-                    Console.Write(FS);
-                    Console.Write(assemblyNameSpaceTypeDetailDict[TYPE_NAME]);
-                }
-                Console.WriteLine();
-            }
+            List<string> cmdLineArgs = args.ToList ();
 
-            List<string> extLibAssemblyList = new List<string> {
-                "Newtonsoft.Json"
-            };
+            // getStdLibAssemblyList (appName);
 
-            List<Dictionary<string, string>> assemblyNameSpaceTypeSummaryList = getExtLibAssemblyList (extLibAssemblyList);
+            List<Dictionary<string, string>> assemblyNameSpaceTypeSummaryList = getStdLibAssemblyList (appName);
 
             foreach (Dictionary<string, string> assemblyNameSpaceTypeDetailDict in assemblyNameSpaceTypeSummaryList) {
-                {
-                    Console.Write (assemblyNameSpaceTypeDetailDict[ASSEMBLY_NAME]);
-                    Console.Write (FS);
-                    Console.Write (assemblyNameSpaceTypeDetailDict[NAMESPACE_NAME]);
-                    Console.Write (FS);
-                    Console.Write (assemblyNameSpaceTypeDetailDict[TYPE_NAME]);
+
+                foreach (string key in assemblyNameSpaceTypeDetailDict.Keys) {
+                    {
+                        Console.Write (assemblyNameSpaceTypeDetailDict[key]);
+                        Console.Write (FS);
+                        Console.Write (assemblyNameSpaceTypeDetailDict[key]);
+                        Console.Write (FS);
+                        Console.Write (assemblyNameSpaceTypeDetailDict[key]);
+                    }
+                    Console.WriteLine ();
                 }
-                Console.WriteLine ();
             }
+
+            // List<string> extLibAssemblyList = new List<string> {
+            //     "Newtonsoft.Json"
+            // };
+
+            // List<Dictionary<string, string>> assemblyNameSpaceTypeSummaryList = getExtLibAssemblyList (extLibAssemblyList);
+
+            // foreach (Dictionary<string, string> assemblyNameSpaceTypeDetailDict in assemblyNameSpaceTypeSummaryList) {
+            //     {
+            //         Console.Write (assemblyNameSpaceTypeDetailDict[ASSEMBLY_NAME]);
+            //         Console.Write (FS);
+            //         Console.Write (assemblyNameSpaceTypeDetailDict[NAMESPACE_NAME]);
+            //         Console.Write (FS);
+            //         Console.Write (assemblyNameSpaceTypeDetailDict[TYPE_NAME]);
+            //     }
+            //     Console.WriteLine ();
+            // }
         }
     }
 }
