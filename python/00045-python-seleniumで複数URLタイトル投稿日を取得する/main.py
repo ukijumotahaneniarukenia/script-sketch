cddf968dbc7ac170375999390b3ec619e4da85a1
ukijumotahaneniarukenia <mrchildrenkh1008@gmail.com>
2020-09-08 06:45:31 +0900

    sketch

--- python/00045-python-seleniumで複数URLタイトル投稿日を取得する/main.py
+++ python/00045-python-seleniumで複数URLタイトル投稿日を取得する/main.py
@@ -11,8 +11,10 @@ import shutil
 
 import getpass
 
-#from selenium import webdriver
-#from selenium.webdriver.chrome.options import Options
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+SITE_NAME = 'サイト名'
 
 TITLE_NAME = 'タイトル名'
 
@@ -22,12 +24,6 @@ UPDATE_TIME = '更新時分'
 
 UPDATE_DATE_TIME = '更新日時'
 
-#ファイルから読み込むようにする
-SITE_URL_NAME_DICT = {
-        'https://www.nikkei.com/technology/ai/' : '日経新聞'
-        ,'https://mainichi.jp/technology/' : '毎日新聞'
-        }
-
 #インデックス位置が変化するDOMのうち一つ上のDOMを選択するようにする
 
 #取得したい項目単位に正規表現を設定してもいいか特に日付関連項目
@@ -35,9 +31,14 @@ SITE_URL_NAME_DICT = {
 #ページャがあるかないか
 
 #ファイルから読み込むようにする
+#２次元配列をdict in dict listに変更する
+
 SITE_URL_XPATH_DICT_LIST = {
     'https://www.nikkei.com/technology/ai/': [
                                {
+                                   SITE_NAME : '日経新聞'
+                               }
+                               ,{
                                    TITLE_NAME : '/html/body/div[9]/div/div/div/div[3]/div[2]/h3/a/span'
                                }
                                ,{
@@ -49,6 +50,9 @@ SITE_URL_XPATH_DICT_LIST = {
                              ]
     ,'https://mainichi.jp/technology/': [
                                {
+                                   SITE_NAME : '毎日新聞'
+                               }
+                               ,{
                                    TITLE_NAME : '/html/body/div[2]/div[1]/div[1]/div/div[2]/ul/li[1]/article/a/h2'
                                }
                                ,{
@@ -70,7 +74,7 @@ DTM = datetime.datetime.today().strftime('%Y-%m-%dT%H-%M-%S')
 
 DST = os.getcwd() #実行ディレクトリ
 
-wait_time_seconds=45
+WAIT_TIME_SECONDS=45
 
 def usage():
     filename = re.sub('.*/', '', __file__)
@@ -92,11 +96,11 @@ Usage:
 def main():
 
     #preprocess
-    #options = webdriver.ChromeOptions()
-    #options.add_argument('--headless')
-    #options.add_argument('/usr/local/src/chromedriver_linux64/chromedriver')
-    #options.add_argument('/usr/local/src/chrome-linux/chrome')
-    #driver = webdriver.Chrome(options=options)
+    options = webdriver.ChromeOptions()
+#    options.add_argument('--headless') #コマンドライン引数上でデバッグモードとして指定できるようにする
+    options.add_argument('/usr/local/src/chromedriver_linux64/chromedriver')
+    options.add_argument('/usr/local/src/chrome-linux/chrome')
+    driver = webdriver.Chrome(options=options)
 
     for site_url,xpath_dict_list in SITE_URL_XPATH_DICT_LIST.items():
 
@@ -110,7 +114,16 @@ def main():
 
             print(site_url)
 
-            #driver.get(root_url)
+            driver.get(site_url)
+
+            time.sleep(WAIT_TIME_SECONDS) #レンダラ完了するまで待つ
+
+            if SITE_NAME in xpath_dict:
+
+                #各取得内容に応じて関数化して前処理メイン処理後処理に工程を分ける
+
+                pass
+
 
             if list(xpath_dict.keys())[0] == TITLE_NAME:
 
@@ -141,11 +154,10 @@ def main():
 
             print(site_name,site_url,xpath_dict,list(xpath_dict.keys())[0],sep='\t')
 
-            #time.sleep(wait_time_seconds) #TODO レンダラ完了するまで待つ
 
             print('初期表示が完了しました')
 
-    #driver.quit()
+    driver.quit()
 
 
 if __name__ == '__main__':
