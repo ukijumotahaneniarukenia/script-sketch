3f136582c7ff5cea8a4f7c1241bb2058539c48c8
ukijumotahaneniarukenia <mrchildrenkh1008@gmail.com>
2020-12-04 23:53:29 +0900

    sketch

--- jq/b.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-echo ポルコ 紅の豚 ジーナ マルコ| while read l;do
-  r=2
-  cmd="echo $l | jq -s -cRr  'split(\" \")|map(sub(\"\n$\";\"\")) as \$a|(\$a|length) as \$n|[range(1;(\$n+1))]|[combinations($r)|sort]|unique|map(select(unique|length==$r))|.[]|map(\$a[.-1])|join(\"\t\")'"
-  echo $cmd
-done
