c92d7f7ba5bf11cdaad709c5267b260c9d3d813c
ukijumotahaneniarukenia <mrchildrenkh1008@gmail.com>
2020-12-12 16:02:35 +0900

    sketch

--- golang/00063-golang-ファイル一覧取得/app/src/main/main.go
+++ golang/00063-golang-ファイル一覧取得/app/src/main/main.go
@@ -1,9 +1,33 @@
 package main
 
 import (
-	"fmt"
+	"io/ioutil"
+	"path/filepath"
 )
 
+func walkAroundAround(dirname string) error {
+	dirList, err := ioutil.ReadDir(dirname)
+
+	if err != nil {
+		return err
+	}
+
+	for _, v := range dirList {
+		fullPath := filepath.Join(dirname, v.Name())
+		if v.IsDir() {
+			// ディレクトリの場合
+			walkAroundAround(fullPath)
+		} else {
+			// ディレクトリ以外の場合
+			println(fullPath)
+		}
+	}
+	return nil
+}
+
 func main() {
-	fmt.Println("Now On Air")
+	err := walkAroundAround(`/home/aine/script-sketch/golang`) // more than one character in rune literal ==> シングルクォートではなくバッククォートで囲む
+	if err != nil {
+		println(err.Error())
+	}
 }
