491b7ddf83a85849ce787b7cf8fd034521bf16c6
ukijumotahaneniarukenia <mrchildrenkh1008@gmail.com>
2021-01-12 20:44:39 +0900

    sketch

--- rust/00001-rust-サンプルプロジェクト/app/src/main.rs
+++ rust/00001-rust-サンプルプロジェクト/app/src/main.rs
@@ -1,3 +1,11 @@
+use std::io::{self, BufRead};
 fn main() {
-    println!("Hello, world!");
+    // https://users.rust-lang.org/t/problem-with-split-in-map-function/41705/3
+    let stdin = io::stdin();
+    let mut row_list = Vec::<Vec<String>>::new();
+    for line in stdin.lock().lines() {
+        let col_list: Vec<_> = line.ok().unwrap().split(' ').map(str::to_owned).collect::<Vec<_>>();
+        row_list.push(col_list);
+    }
+    println!("{:?}", row_list);
 }
