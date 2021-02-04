a2fba06057b74675831e0de1606b21bd1c70bc9e
ukijumotahaneniarukenia <mrchildrenkh1008@gmail.com>
2021-02-04 17:41:54 +0900

    sketch

---
+++ Vue/00010-Vue-jQueryを使う/app/pages/index.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <ul>
+      <li v-for="(item, key) of items" :key="key">
+        <nuxt-link :to="item.to">
+          <b-icon :icon="item.icon" /> {{ item.title }}
+        </nuxt-link>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      items: [
+        {
+          title: "Home",
+          icon: "home",
+          to: { name: "index" },
+        },
+        {
+          title: "sketch-jquery",
+          icon: "lightbulb",
+          to: { name: "sketch-jquery" },
+        },
+      ],
+    };
+  },
+};
+</script>
---
+++ Vue/00010-Vue-jQueryを使う/app/pages/sketch/jquery/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <button id="btn">クリックでテキスト挿入</button>
+    <p id="hogehoge"></p>
+  </div>
+</template>
+
+<script>
+export default {
+  mounted: function () {
+    $("#btn").click(function () {
+      $("#hogehoge").text("テキスト挿入");
+    });
+  },
+};
+</script>
