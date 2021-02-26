6e0e1360f869a5a8aface62ee9fedf3aa3a63e1e
ukijumotahaneniarukenia <mrchildrenkh1008@gmail.com>
2021-02-27 02:13:37 +0900

    sketch

--- Vue/00007-Vue-サンドボックス/app/pages/video-cutframe/index.vue
+++ Vue/00007-Vue-サンドボックス/app/pages/video-cutframe/index.vue
@@ -5,6 +5,7 @@
       <input type="file" accept="video/*" @change="detectUploadFile($event)" />
       <p id="progress"></p>
     </div>
+    <div>frameCutList: {{ frameCutList.length }}</div>
   </div>
 </template>
 
@@ -16,13 +17,13 @@ export default {
   data() {
     return {
       frameCutList: [],
-      uploadFile: {},
+      uploadFile: {}
     };
   },
   methods: {
     detectUploadFile(event) {
       const targetUploadFileList = Array.from(event.target.files);
-      targetUploadFileList.map((item) => {
+      targetUploadFileList.map(item => {
         this.uploadFile = item;
         this.extractFrames(this.uploadFile);
       });
@@ -31,74 +32,91 @@ export default {
       let video = document.createElement("video");
       let array = [];
       let canvas = document.createElement("canvas");
-      let ctx = canvas.getContext("2d");
+      let canvasContext = canvas.getContext("2d");
       let pro = document.querySelector("#progress");
 
+      video.muted = true;
+      video.src = URL.createObjectURL(targetFile);
+      video.play(); // イベントリスナ登録処理がデフォルトで先に行われることの証拠
       function initCanvas(event) {
-        console.log("initCanvas", event)
+        console.log("initCanvas", event);
         canvas.width = video.videoWidth;
         canvas.height = video.videoHeight;
       }
 
-      function drawFrame(event, targetVideoDom, targetCanvasDom, targetCanvasContext) {
-        console.log("drawFrame", event)
+      function drawFrame(
+        event,
+        targetVideoDom,
+        targetCanvasDom,
+        targetCanvasContext
+      ) {
+        console.log("drawFrame", event);
         targetVideoDom.pause();
         targetCanvasContext.drawImage(targetVideoDom, 0, 0);
         // https://developer.mozilla.org/ja/docs/Web/API/HTMLCanvasElement/toBlob
         // https://developer.mozilla.org/en-US/docs/Web/API/HTMLCanvasElement/toBlob#Polyfill
         // これ便利 toData
-        targetCanvasDom.toBlob((blob) => {console.log("blob",blob)}, "image/jpeg");
+        targetCanvasDom.toBlob(blob => {
+          // console.log("blob",blob)
+          saveFrame(blob);
+        }, "image/jpeg");
         pro.innerHTML =
-          ((targetVideoDom.currentTime / targetVideoDom.duration) * 100).toFixed(2) + " %";
+          (
+            (targetVideoDom.currentTime / targetVideoDom.duration) *
+            100
+          ).toFixed(2) + " %";
         if (targetVideoDom.currentTime < targetVideoDom.duration) {
           targetVideoDom.play();
         }
       }
 
-      function saveFrame(blob) {
-        array.push(blob);
+      function saveFrame(targetBlob) {
+        console.log("saveFrame", targetBlob);
+        array.push(targetBlob);
       }
 
-      function revokeURL(event) {
-        URL.revokeObjectURL(this.src);
-      }
-
-      function onend(event) {
+      function onend(event, targetVideoDom) {
         let img;
-        // do whatever with the frames
+        // blobデータからimageタグを生成
         for (let i = 0; i < array.length; i++) {
           img = new Image();
-          img.onload = revokeURL;
+          img.onload = event => {
+            console.log("img tag detect src property change", event);
+          };
           img.src = URL.createObjectURL(array[i]);
           document.body.appendChild(img);
         }
-        // we don't need the video's objectURL anymore
-        URL.revokeObjectURL(this.src);
+        URL.revokeObjectURL(targetVideoDom.src);
       }
 
-      video.muted = true;
-
-      video.addEventListener("loadedmetadata", (event)=>{
-        initCanvas(event)
-        // console.log(canvas)
-        }
-        , false
+      video.addEventListener(
+        "loadedmetadata",
+        event => {
+          initCanvas(event);
+          // console.log(canvas)
+        },
+        false
       );
       // クロージャじゃないとだめ
       // timeupdateイベント発火時にpause＆playする
       // https://developer.mozilla.org/ja/docs/Web/API/HTMLMediaElement/timeupdate_event
-      video.addEventListener("timeupdate", (event) => {
-        drawFrame(event, video, canvas, ctx)
-        }, false
+      video.addEventListener(
+        "timeupdate",
+        event => {
+          drawFrame(event, video, canvas, canvasContext);
+        },
+        false
       );
-      // video.addEventListener("ended", onend, false);
-
-      video.src = URL.createObjectURL(targetFile);
-      video.play();
-    },
-  },
+      video.addEventListener(
+        "ended",
+        event => {
+          onend(event, video);
+        },
+        false
+      );
+    }
+  }
 };
 </script>
 
-<style>
-</style>
+<style></style>
