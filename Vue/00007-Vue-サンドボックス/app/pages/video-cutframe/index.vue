b0fefb039f322698f3c23ae1aa0f0ce783f73dfb
ukijumotahaneniarukenia <mrchildrenkh1008@gmail.com>
2021-02-26 10:16:44 +0900

    sketch

---
+++ Vue/00007-Vue-サンドボックス/app/pages/video-cutframe/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <!-- https://stackoverflow.com/questions/32699721/javascript-extract-video-frames-reliably -->
+  <div>
+    <div>
+      <input type="file" accept="video/*" />
+      <p id="progress"></p>
+    </div>
+  </div>
+</template>
+
+<script>
+// https://github.com/eligrey/FileSaver.js/
+import { saveAs } from 'file-saver';
+
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
